2f73582f35f890e280d9c14a171e3d983f38f7fa
danil <danil@Solid>
2010-07-01 11:24:12 +0300

    first commit

---
+++ src/com/puppycrawl/tools/checkstyle/checks/naming/AbstractCheck1.java
@@ -0,0 +1,10 @@
+package com.puppycrawl.tools.checkstyle.checks.naming;
+
+public class AbstractCheck1 {
+	int color;
+	int startPoint;
+
+	public void draw() {
+		// no code
+	}
+}
---
+++ src/com/puppycrawl/tools/checkstyle/checks/naming/Check2.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.naming;
+
+public abstract class Check2 {
+	int color;
+	int startPoint;
+
+	public abstract void draw();
+}
