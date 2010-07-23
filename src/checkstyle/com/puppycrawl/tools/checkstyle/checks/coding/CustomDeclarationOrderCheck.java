3dcd969e6da0e4f3cc4a94d3842ae69eeba7aec7
Danil <Danil_Crimae@mail.ru>
2010-07-23 20:08:39 +0300

    Parsing RegEx in CustomDeclarationOrderCheck has done.

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/CustomDeclarationOrderCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/CustomDeclarationOrderCheck.java
@@ -1,36 +1,20 @@
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.commons.beanutils.ConversionException;
 
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.api.Utils;
 
 public class CustomDeclarationOrderCheck extends Check
 {
-	 /** State for the VARIABLE_DEF */
-    private static final int STATE_VARIABLE_DEF = 1;
-
-    /** State for the VARIABLE_DEF */
-    private static final int STATE_ANNOTATION_VARIABLE_DEF = 2;
-
-    /** State for the CTOR_DEF */
-    private static final int STATE_CTOR_DEF = 3;
-
-    /** State for the METHOD_DEF */
-    private static final int STATE_METHOD_DEF = 4;
-    
     /** List of order declaration customizing by user */
-    private final ArrayList<String> mCustomOrderDeclaration = new ArrayList<String>();
-    
-	/**
-	 * @param aInputOrderDeclaration String line with the user custom declaration 
-	 * @return the mCustomOrderDeclaration massive of declaration
-	 */
-	public ArrayList<String> setCustomOrderDeclaration(String aInputOrderDeclaration) {
-		
-		return mCustomOrderDeclaration;
-	}
+    private final ArrayList<FormatMatcher> mCustomOrderDeclaration = new ArrayList<FormatMatcher>();
     
 	@Override
 	public int[] getDefaultTokens() {
@@ -51,5 +35,110 @@ public class CustomDeclarationOrderCheck extends Check
            // mScopeStates.pop();
         }
 	}
+	
+	/**
+	 * Parsing input line with custom declaration of order into massive
+	 * @param aInputOrderDeclaration The string line with the user custom declaration 
+	 */
+	public void setCustomDeclarationOrder(String aInputOrderDeclaration) {
+		for (String currentState : aInputOrderDeclaration.split("\\s*###\\s*")) {
+			mCustomOrderDeclaration.add(new FormatMatcher(currentState));
+		}
+	}
+		
+    /**
+     * Set whether or not the match is case sensitive.
+     * @param aCaseInsensitive true if the match is case insensitive.
+     */
+	public void setIgnoreCase(boolean aCaseInsensitive)
+	{
+		if (aCaseInsensitive) {
+			for (FormatMatcher currentRule : mCustomOrderDeclaration) {
+				currentRule.setCompileFlags(Pattern.CASE_INSENSITIVE);
+			}
+		}
+	}
+    
+    /**
+     * private class for members of class and their patterns
+     */
+	private static class FormatMatcher
+    {
+	 /** mClassMember position in parsed input massive*/
+	 private static final int mClassMemberPosition = 0;
+	 /** mRegExp position in parsed input massive*/
+	 private static final int mRegExpPosition = 1; 
+     /** The regexp to match against */
+     private Pattern mRegExp;
+     /** The Member of Class*/
+     private String mClassMember;
+     /** The string format of the RegExp */
+     private String mFormat;
+     
+     /**
+      * Creates a new <code>FormatMatcher</code> instance. 
+      * Parse into Definition and RegEx.
+      * Defaults the compile flag to 0 (the default).
+      * @param aInputRule input string with ClassDefinition and RegEx
+      * @throws ConversionException unable to parse aDefaultFormat
+      */
+     public FormatMatcher(String aInputRule)
+         throws ConversionException
+     {
+         this(aInputRule, 0);
+     }
+
+     /**
+      * Creates a new <code>FormatMatcher</code> instance.
+      * @param aInputRule input string with ClassDefinition and RegEx
+      * @param aCompileFlags the Pattern flags to compile the regexp with.
+      * See {@link Pattern#compile(java.lang.String, int)}
+      * @throws ConversionException unable to parse aDefaultFormat
+      */
+     public FormatMatcher(final String aInputRule, final int aCompileFlags)
+         throws ConversionException, ArrayIndexOutOfBoundsException
+     {
+    	 String inputRule[] = aInputRule.split("[()]");
+    	 String aDefaultFormat = null;
+    	 mClassMember = inputRule[mClassMemberPosition];
+    	 if (inputRule.length < 2)
+    		// if RegExp is null
+    		 aDefaultFormat = "$^"; // the empty RegExp
+    	 else aDefaultFormat = inputRule[mRegExpPosition];
+         updateRegexp(aDefaultFormat, aCompileFlags);
+     }
+
+     /** @return the regexp to match against */
+     public final Pattern getRegexp()
+     {
+         return mRegExp;
+     }
+     
+     /**
+      * Set the compile flags for the regular expression.
+      * @param aCompileFlags the compile flags to use.
+      */
+     public final void setCompileFlags(int aCompileFlags)
+     {
+         updateRegexp(mFormat, aCompileFlags);
+     }
+
+     /**
+      * Updates the regular expression using the supplied format and compiler
+      * flags. Will also update the member variables.
+      * @param aFormat the format of the regular expression.
+      * @param aCompileFlags the compiler flags to use.
+      */
+     private void updateRegexp(String aFormat, int aCompileFlags)
+     {
+         try {
+             mRegExp = Utils.getPattern(aFormat, aCompileFlags);
+             mFormat = aFormat;
+         }
+         catch (final PatternSyntaxException e) {
+             throw new ConversionException("unable to parse " + aFormat, e);
+         }
+     }
+ }
 
 }
