36e1a61d510b9a099e8a48a56dc56e1a53c7bc70
Danil <Danil_Crimae@mail.ru>
2010-08-01 02:50:40 +0300

    JUnitTest to CustomDeclarationOrderCheck was written. Messages to localization was added.

---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/CustomDeclarationOrderCheckTest.java
@@ -0,0 +1,43 @@
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+public class CustomDeclarationOrderCheckTest extends BaseCheckTestSupport {
+
+    @Test
+    public void testCustomDecrationOrder() throws Exception
+    {
+        final DefaultConfiguration checkConfig = createCheckConfig(CustomDeclarationOrderCheck.class);
+        final String[] expected = {
+                "18:5: Field definition in wrong order.",
+                "22:5: Field definition in wrong order.",
+                "24:5: Field definition in wrong order.",
+                "44:5: Constructor definition in wrong order.",
+                "56:9: Field definition in wrong order.",
+                "81:9: Class definition in wrong order.",
+                "93:5: Method definition in wrong order." };
+        System.setProperty("testinputs.dir",
+                "/home/danil/workspace/my/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle");
+        checkConfig.addAttribute("customDeclarationOrder",
+                                  "Field(.*final.*public|.*public.*final) ###"
+                                + "Field(@SuppressWarnings(.*serial.*)) ###"
+                                + "Field(@Rule) ###"
+                                + "Field(protected) ### "
+                                + "Field(private) ###"
+                                + "CTOR()###"
+                                + "Method() ###"
+                                + "Method(.*abstract.*public|.*public.*abstract|protected) ###"
+                                + "InnerClass (public.*abstract) ###"
+                                + "InnerClass (private)");
+        checkConfig.addAttribute("ignoreRegExCase", "true");
+        verify(checkConfig, getPath("coding" + File.separator
+                + "InputCustomDeclarationOrder.java"), expected);
+        checkConfig.addAttribute("customDeclarationOrder", "Field .*final.*public|.*public.*final)");
+    }
+
+}
