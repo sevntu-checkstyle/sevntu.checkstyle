b9abfede329ee075b601ae8b698e294648d24dd5
Danil <Danil_Crimae@mail.ru>
2010-08-10 16:05:11 +0300

    Console analizer was done.

--- Checker for new checks/RunChecking.sh
+++ Checker for new checks/RunChecking.sh
@@ -1,20 +1,26 @@
-echo "Input path of project:"
+#!/bin/bash
+echo -n "Input path of project:"
 read path
-rmdir Out --ignore-fail-on-non-empty
-sleep 1s
+rm -r ./Out
 mkdir Out
 echo "Running AbstractClassNameCheck:"
-java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c AbstractClassNameCheckConfig.xml -r $path > ./Out/AbstractClassNameCheck.txt
-cat -n ./Out/AbstractClassNameCheck.txt
+java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ./Configs/AbstractClassNameCheckConfig.xml -r $path > ./Out/AbstractClassNameCheck.txt
+cat ./Out/AbstractClassNameCheck.txt
+echo
 echo "Running CustomDeclarationOrderCheck:"
-java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ConfigCustomDeclarationOrderCheck.xml -r $path > ./Out/CustomDeclarationOrderCheck.txt
-cat -n ./Out/CustomDeclarationOrderCheck.txt
+java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ./Configs/ConfigCustomDeclarationOrderCheck.xml -r $path > ./Out/CustomDeclarationOrderCheck.txt
+cat ./Out/CustomDeclarationOrderCheck.txt
+echo
 echo "Running InnerClassCheck:"
-java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ConfigInnerClassCheck.xml -r $path > ./Out/InnerClassCheck.txt
-cat -n ./Out/InnerClassCheck.txt
+java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ./Configs/ConfigInnerClassCheck.xml -r $path > ./Out/InnerClassCheck.txt
+cat ./Out/InnerClassCheck.txt
+echo
 echo "Running LineLengthCheck:"
-java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c LineLengthCheckConfig.xml -r $path > ./Out/LineLengthCheck.txt
-cat -n ./Out/LineLengthCheck.txt
+java -classpath checkstyle-all-5.1.jar	com.puppycrawl.tools.checkstyle.Main	-c ./Configs/LineLengthCheckConfig.xml -r $path > ./Out/LineLengthCheck.txt
+cat ./Out/LineLengthCheck.txt
+echo
+echo -n "Input any key for exit."
+read
 
 
 
