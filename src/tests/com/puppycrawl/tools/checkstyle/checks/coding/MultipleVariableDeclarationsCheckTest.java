4d632d2ecfd601e732a3564221bb19781e8ffb4c
Daniil_Yaroslavtsev <Daniil.Yaroslavtsev@gmail.com>
2010-11-26 10:55:34 +0200

    MultiplyVariableDeclarations check was added together with the appropriate configuration file.

---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/MultipleVariableDeclarationsCheckTest.java
@@ -0,0 +1,29 @@
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import java.io.File;
+import org.junit.Test;
+
+public class MultipleVariableDeclarationsCheckTest extends BaseCheckTestSupport
+{
+    @Test
+    public void testIt() throws Exception
+    {
+        DefaultConfiguration checkConfig =
+            createCheckConfig(MultipleVariableDeclarationsCheck.class);
+
+        final String[] expected = {
+            "3:5: Each variable declaration must be in its own statement.",
+            "4:5: Only one variable definition per line allowed.",
+            "7:9: Each variable declaration must be in its own statement.",
+            "8:9: Only one variable definition per line allowed.",
+            "12:5: Only one variable definition per line allowed.",
+            "15:5: Only one variable definition per line allowed.",
+            "20:14: Each variable declaration must be in its own statement.",
+        };
+        System.setProperty("testinputs.dir", "/media/Data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+        verify(checkConfig,getPath("InputMultipleVariableDeclarations.java"),expected);
+         
+    }
+}
