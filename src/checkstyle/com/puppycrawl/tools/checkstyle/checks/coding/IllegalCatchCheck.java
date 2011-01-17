3748a511da4c71734a1c444a5d1bbb53637f7818
Daniil_Yaroslavtsev <All.The.World.Admin@gmail.com>
2011-01-17 13:54:14 +0200

    Last version of IllegalCheckCatch for patch creating. JUnittests & configs written. Coverage 100%.

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheck.java
@@ -26,23 +26,51 @@ import com.puppycrawl.tools.checkstyle.checks.CheckUtils;
 /**
  * Catching java.lang.Exception, java.lang.Error or java.lang.RuntimeException
  * is almost never acceptable.
- * 
  * @author <a href="mailto:simon@redhillconsulting.com.au">Simon Harris</a>
  */
 public final class IllegalCatchCheck extends AbstractIllegalCheck
 {
+
+    /** disable warning for "catch" blocks containing
+     * throwing an exception. */
+    private boolean mThrowPermit;
+
+    /** disable warning for "catch" blocks containing
+     * rethrowing an exception. */
+    private boolean mRethrowPermit;
+
+    /**
+     * Enable(false) | Disable(true) warning for "catch" blocks containing
+     * throwing an exception.
+     * @param aValue Disable warning for throwing
+     */
+    public void setThrowPermit(final boolean aValue)
+    {
+        mThrowPermit = aValue;
+    }
+
+    /**
+     * Enable(false) | Disable(true) warning for "catch" blocks containing
+     * rethrowing an exception.
+     * @param aValue Disable warning for rethrowing
+     */
+    public void setRethrowPermit(final boolean aValue)
+    {
+        mRethrowPermit = aValue;
+    }
+
     /** Creates new instance of the check. */
     public IllegalCatchCheck()
     {
-        super(new String[] { "Exception", "Error", "RuntimeException",
-                "Throwable", "java.lang.Error", "java.lang.Exception",
-                "java.lang.RuntimeException", "java.lang.Throwable", });
+        super(new String[]{"Exception", "Error", "RuntimeException",
+            "Throwable", "java.lang.Error", "java.lang.Exception",
+            "java.lang.RuntimeException", "java.lang.Throwable", });
     }
 
     @Override
     public int[] getDefaultTokens()
     {
-        return new int[] { TokenTypes.LITERAL_CATCH };
+        return new int[]{TokenTypes.LITERAL_CATCH};
     }
 
     @Override
@@ -54,13 +82,21 @@ public final class IllegalCatchCheck extends AbstractIllegalCheck
     @Override
     public void visitToken(DetailAST aDetailAST)
     {
-        final DetailAST paramDef = aDetailAST.findFirstToken(TokenTypes.PARAMETER_DEF);
-       // System.out.println("nextNode - line:" + paramDef.getLineNo() + " col:" +paramDef.getColumnNo()+ ", TYPE: " + paramDef.getText());
-        
-        //DetailAST currentNode = paramDef;
-        String str=paramDef.toStringList();
-        boolean noWarning = str.indexOf(" throw[")>=0;           
-            
+        final DetailAST paramDef = aDetailAST
+                .findFirstToken(TokenTypes.PARAMETER_DEF);
+
+        final DetailAST throwAST = getThrowAST(aDetailAST);
+        final boolean noWarning = (throwAST != null
+             && throwAST.getFirstChild().getFirstChild() != null
+             && ((mThrowPermit && throwAST.getFirstChild().getType()
+                     == TokenTypes.EXPR
+            && throwAST.getFirstChild().getFirstChild().getType()
+                     == TokenTypes.IDENT)
+             | (mRethrowPermit && throwAST.getFirstChild().getType()
+                     == TokenTypes.EXPR
+            && throwAST.getFirstChild().getFirstChild().getType()
+                     == TokenTypes.LITERAL_NEW)));
+
         final DetailAST excType = paramDef.findFirstToken(TokenTypes.TYPE);
         final FullIdent ident = CheckUtils.createFullType(excType);
 
@@ -68,29 +104,55 @@ public final class IllegalCatchCheck extends AbstractIllegalCheck
             log(aDetailAST, "illegal.catch", ident.getText());
         }
     }
-    
-//    public DetailAST throwSearch(DetailAST currentNode){
-//                
-//        System.out.println("Current node: line:" + currentNode.getLineNo() + " col:" +currentNode.getColumnNo()+ ", TYPE: " + currentNode.getText());
-//        
-//        if(currentNode.getNumberOfChildren()<=0){ // has not childs 
-//          //   go to next node
-//            DetailAST NextSibling = currentNode.getNextSibling();
-//            if(NextSibling!=null)currentNode=NextSibling;
-//            else return null;
-//        }
-//        
-//        else { // has childs
-//            
-//            if(currentNode.getType()==TokenTypes.LITERAL_THROW)return currentNode;             
-//            
-//            return(throwSearch(currentNode.getFirstChild()));
-//                       
-//        }
-//        
-//        return null;
-//    }
-    
-    
-    
+
+    /** Looking for the keyword "throw" among current (aParentAST) node childs.
+     * @param aParentAST - the current parent node.
+     * @return null if the "throw" keyword was not found
+     * or the LITERAL_THROW DetailAST otherwise
+     */
+    public DetailAST getThrowAST(DetailAST aParentAST)
+    {
+
+        final DetailAST asts[] = getChilds(aParentAST);
+
+        for (DetailAST currentNode : asts) {
+
+            if (currentNode.getType() != TokenTypes.PARAMETER_DEF
+                    && currentNode.getNumberOfChildren() > 0)
+            {
+                final DetailAST astResult = (getThrowAST(currentNode));
+                if (astResult != null) {
+                    return astResult;
+                }
+            }
+
+            if (currentNode.getType() == TokenTypes.LITERAL_THROW) {
+                return currentNode;
+            }
+
+            if (currentNode.getNextSibling() != null) {
+                currentNode = currentNode.getNextSibling();
+            }
+        }
+        return null;
+    }
+
+    /** Gets all the children one level below on the current top node.
+     * @param aNode - current parent node.
+     * @return an array of childs one level below
+     * on the current parent node aNode. */
+    public DetailAST[] getChilds(DetailAST aNode)
+    {
+        final DetailAST[] result = new DetailAST[aNode.getChildCount()];
+
+        DetailAST currNode = aNode.getFirstChild();
+
+        for (int i = 0; i < aNode.getNumberOfChildren(); i++) {
+            result[i] = currNode;
+            currNode = currNode.getNextSibling();
+        }
+
+        return result;
+    }
+
 }
--- src/tests/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheckTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheckTest.java
@@ -8,45 +8,76 @@ import org.junit.Test;
 public class IllegalCatchCheckTest extends BaseCheckTestSupport
 {
     @Test
-    public void testDefault() throws Exception
+    public final void testDefault() throws Exception
     {
         DefaultConfiguration checkConfig = createCheckConfig(IllegalCatchCheck.class);
 
+        
         String[] expected = {
             "9:9: Catching 'RuntimeException' is not allowed.",
-            "11:9: Catching 'Exception' is not allowed.",
-            "13:9: Catching 'Throwable' is not allowed.",
-                
-//          "24:9: Catching 'java.lang.RuntimeException' is not allowed.",
-//          "29:9: Catching 'java.lang.Exception' is not allowed.",
-//          "34:9: Catching 'java.lang.Throwable' is not allowed.",
+            "11:9: Catching 'java.lang.Exception' is not allowed.",
+            "13:9: Catching 'Throwable' is not allowed.",       
+            "24:9: Catching 'RuntimeException' is not allowed.",
+            "29:9: Catching 'java.lang.Exception' is not allowed.",
+            "34:9: Catching 'Throwable' is not allowed.",
         };
-       
-      //  System.setProperty("testinputs.dir", "/home/developer/Daniil Yaroslavtsev/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+
         System.setProperty("testinputs.dir", "/media/Data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+        
+        checkConfig.addAttribute("throwPermit", "false");
+        checkConfig.addAttribute("rethrowPermit", "false");
+        
         //verify(checkConfig, getPath("coding" + File.separator + "InputIllegalCatchCheck.java"), expected);
-        verify(checkConfig,getPath("InputIllegalCatchCheck.java"),expected);
+        verify(checkConfig,getPath("InputIllegalCatchCheckNew.java"),expected);
     }
 
     @Test
-    public void testIllegalClassNames() throws Exception
+    public final void testThrowPermit() throws Exception
+    {
+        DefaultConfiguration checkConfig = createCheckConfig(IllegalCatchCheck.class);
+
+
+        String[] expected = {
+            "9:9: Catching 'RuntimeException' is not allowed.",
+            "11:9: Catching 'java.lang.Exception' is not allowed.",
+            "13:9: Catching 'Throwable' is not allowed.",
+//          "24:9: Catching 'RuntimeException' is not allowed.",
+            "29:9: Catching 'java.lang.Exception' is not allowed.",
+            "34:9: Catching 'Throwable' is not allowed.",
+        };
+
+        System.setProperty("testinputs.dir", "/media/Data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+
+        checkConfig.addAttribute("throwPermit", "true");
+        checkConfig.addAttribute("rethrowPermit", "false");
+        
+        //verify(checkConfig, getPath("coding" + File.separator + "InputIllegalCatchCheck.java"), expected);
+        verify(checkConfig,getPath("InputIllegalCatchCheckNew.java"),expected);
+    }
+    
+    @Test
+    public final void testReThrowPermit() throws Exception
     {
         DefaultConfiguration checkConfig = createCheckConfig(IllegalCatchCheck.class);
         checkConfig.addAttribute("illegalClassNames",
                                  "java.lang.Error, java.lang.Exception, java.lang.Throwable");
 
         String[] expected = {
-            "11:9: Catching 'Exception' is not allowed.",
+//          "9:9: Catching 'RuntimeException' is not allowed.",
+            "11:9: Catching 'java.lang.Exception' is not allowed.",
             "13:9: Catching 'Throwable' is not allowed.",
-                
-         // "29:9: Catching 'java.lang.Exception' is not allowed.",
-         // "34:9: Catching 'java.lang.Throwable' is not allowed.",
+//          "24:9: Catching 'RuntimeException' is not allowed.",
+//          "29:9: Catching 'java.lang.Exception' is not allowed.",
+//          "34:9: Catching 'Throwable' is not allowed.",
         };
 
-       // System.setProperty("testinputs.dir", "/home/developer/Daniil Yaroslavtsev/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
         System.setProperty("testinputs.dir", "/media/Data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
-        
+
+        checkConfig.addAttribute("throwPermit", "false");
+        checkConfig.addAttribute("rethrowPermit", "true");
+
         //verify(checkConfig, getPath("coding" + File.separator + "InputIllegalCatchCheck.java"), expected);
-        verify(checkConfig,getPath("InputIllegalCatchCheck.java"),expected);
+        verify(checkConfig,getPath("InputIllegalCatchCheckNew.java"),expected);
     }
+
 }
