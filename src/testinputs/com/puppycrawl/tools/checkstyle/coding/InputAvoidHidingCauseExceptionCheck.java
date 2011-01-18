127b884cb85c3884556b50edc5b55db186b3d24b
Daniil_Yaroslavtsev <Daniil.Yaroslavtsev@gmail.com>
2011-01-18 14:27:41 +0200

    New configs added!

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/coding/InputAvoidHidingCauseExceptionCheck.java
@@ -0,0 +1,5 @@
+package com.puppycrawl.tools.checkstyle.coding;
+
+public class InputAvoidHidingCauseExceptionCheck {
+
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/AvoidHidingCauseExceptionCheckTest.java
@@ -0,0 +1,5 @@
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+public class AvoidHidingCauseExceptionCheckTest {
+
+}
