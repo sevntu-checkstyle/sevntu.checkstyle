60ba045e87760b1e45e7970b53a0a455a3490063
selden <selden@Selden-PC>
2011-02-09 09:20:17 +0200

    InputAvoidNotShortCircuitOperatorsForBoolean check is now ready to build/test

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNotShortCircuitOperatorsForBooleanCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNotShortCircuitOperatorsForBooleanCheck.java
@@ -18,6 +18,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
+import java.util.LinkedList;
+
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.api.Check;
@@ -26,45 +28,145 @@ import com.puppycrawl.tools.checkstyle.checks.CheckUtils;
 /**
  * <p>
  * This check prevents ...
+ * 
  * @author <a href="mailto:Daniil.Yaroslavtsev@gmail.com"> Daniil
  *         Yaroslavtsev</a>
  */
-public class AvoidNotShortCircuitOperatorsForBooleanCheck extends Check
-{
+public class AvoidNotShortCircuitOperatorsForBooleanCheck extends Check {
 
     /** Creates new instance of the check. */
-    public AvoidNotShortCircuitOperatorsForBooleanCheck()
-    {
+    public AvoidNotShortCircuitOperatorsForBooleanCheck() {
     }
 
     @Override
-    public int[] getDefaultTokens()
-    {
-        return new int[] {TokenTypes.BOR, TokenTypes.BAND,
-           TokenTypes.BOR_ASSIGN, TokenTypes.BAND_ASSIGN, };
+    public int[] getDefaultTokens() {
+        return new int[] { TokenTypes.BOR, TokenTypes.BAND,
+                TokenTypes.BOR_ASSIGN, TokenTypes.BAND_ASSIGN, };
     }
 
     @Override
-    public void visitToken(DetailAST aDetailAST)
-    {
+    public void visitToken(final DetailAST aDetailAST) {
+
+//        System.out.println("Current node:\"" + aDetailAST.getText()
+//                + "\". Line: " + aDetailAST.getLineNo() + ", column: "
+//                + aDetailAST.getColumnNo());
+
         DetailAST currentNode = aDetailAST;
         while (currentNode != null
                 && currentNode.getType() != TokenTypes.LITERAL_IF
-                && currentNode.getType() != TokenTypes.LITERAL_FOR
+                && currentNode.getType() != TokenTypes.FOR_CONDITION
                 && currentNode.getType() != TokenTypes.LITERAL_WHILE
+                && currentNode.getType() != TokenTypes.LITERAL_RETURN
                 && currentNode.getType() != TokenTypes.VARIABLE_DEF
-                && currentNode.getType() != TokenTypes.CLASS_DEF)
-        {
+                && currentNode.getType() != TokenTypes.METHOD_DEF
+                && currentNode.getType() != TokenTypes.CLASS_DEF) {
             currentNode = currentNode.getParent();
         }
 
-        if (!(currentNode.getType() == TokenTypes.VARIABLE_DEF && "int".equals(CheckUtils
-                        .createFullType(currentNode.findFirstToken(TokenTypes.TYPE))
-                        .getText()))) {
-            log(aDetailAST, "avoid.not.short.circuit.operators.for.boolean",
-                    aDetailAST.getText());
+        int type = currentNode.getType();
+//        System.out.println("This node:\"" + currentNode.getText()+ "\". Line: " + currentNode.getLineNo() + ", column: "
+//                + currentNode.getColumnNo());
+
+        if (type != TokenTypes.METHOD_DEF
+                && type != TokenTypes.CLASS_DEF) {
+
+            if (type == TokenTypes.VARIABLE_DEF && isBoolean(currentNode)) {
+                log(aDetailAST,"avoid.not.short.circuit.operators.for.boolean",aDetailAST.getText());
+            } 
+            
+            else if (type == TokenTypes.LITERAL_RETURN){ // "return" situation
+
+                while (currentNode != null
+                        && currentNode.getType() != TokenTypes.METHOD_DEF
+                        && currentNode.getType() != TokenTypes.CLASS_DEF) {
+                    currentNode = currentNode.getParent();
+                }
+
+                if(currentNode.getType() == TokenTypes.METHOD_DEF
+                        && isBoolean(currentNode)){
+                    log(aDetailAST,"avoid.not.short.circuit.operators.for.boolean",aDetailAST.getText());
+                }
+            }
+
+            else if (type == TokenTypes.LITERAL_IF
+                 || type == TokenTypes.LITERAL_WHILE
+                 || type == TokenTypes.FOR_CONDITION){ // "for/if/while" situation
+
+                if(isNotInteger(aDetailAST)){
+                log(aDetailAST,"avoid.not.short.circuit.operators.for.boolean",aDetailAST.getText());
+                }
+
+            }
+        }
+    }
+
+    public boolean isBoolean(DetailAST aNode) {
+        return "boolean".equals(CheckUtils.createFullType(
+                aNode.findFirstToken(TokenTypes.TYPE)).getText());
+    }
+
+    public boolean isIntegerType(DetailAST aNode) {
+        return "int".equals(CheckUtils.createFullType(
+                aNode.findFirstToken(TokenTypes.TYPE)).getText());
+    }
+    
+    public boolean isNotInteger(DetailAST aNode) {
+        
+        LinkedList<String> childNames = new LinkedList<String>();
+        LinkedList<String> integerVariablesNames = new LinkedList<String>();
+
+        for(DetailAST child:getChilds(aNode)){
+            childNames.add(child.getText());
         }
 
+        while (aNode != null
+                && aNode.getType() != TokenTypes.CTOR_DEF
+                && aNode.getType() != TokenTypes.METHOD_DEF
+                && aNode.getType() != TokenTypes.CLASS_DEF) {
+            aNode = aNode.getParent();
+        }
+
+        if (aNode.getType() != TokenTypes.CTOR_DEF
+                || aNode.getType() != TokenTypes.CLASS_DEF
+                || aNode.getType() != TokenTypes.METHOD_DEF) {
+
+            for (DetailAST currentNode : getChilds(aNode.getLastChild())) {
+                if (currentNode.getType() == TokenTypes.VARIABLE_DEF){
+
+                    if (isIntegerType(currentNode)) {
+                        integerVariablesNames.add(currentNode.findFirstToken(TokenTypes.IDENT).getText());
+                    }
+                }
+            }
+
+            for(String name:childNames){
+                if(integerVariablesNames.contains(name)){
+                    return false;
+                }
+
+            }
+
+        }
+
+        return true;
+    }
+
+    /** Gets all the children one level below on the current top node.
+     * @param aNode - current parent node.
+     * @return an array of childs one level below
+     * on the current parent node aNode. */
+    public DetailAST[] getChilds(DetailAST aNode)
+    {
+        final DetailAST[] result = new DetailAST[aNode.getChildCount()];
+
+        DetailAST currNode = aNode.getFirstChild();
+
+        for (int i = 0; i < aNode.getNumberOfChildren(); i++) {
+            result[i] = currNode;
+            currNode = currNode.getNextSibling();
+        }
+
+        return result;
     }
 
 }
--- src/tests/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNotShortCircuitOperatorsForBooleanCheckTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNotShortCircuitOperatorsForBooleanCheckTest.java
@@ -6,29 +6,33 @@ import java.io.File;
 import org.junit.Test;
 
 public class AvoidNotShortCircuitOperatorsForBooleanCheckTest extends BaseCheckTestSupport {
-    final String message1 = "Not short-circuit Operator '";
-    final String message2 = "' used.";
+
     
     @Test
     public final void testAll() throws Exception {
         DefaultConfiguration checkConfig = createCheckConfig(AvoidNotShortCircuitOperatorsForBooleanCheck.class);
 
         String[] expected = {
-            "5:17: "+message1+"|"+message2,
-            "9:25: "+message1+"|"+message2,
-        //  "10:23: "+message1+"|"+message2,
-            "11:15: "+message1+"|"+message2,
-            "15:12: "+message1+"|="+message2,
-            "16:14: "+message1+"&"+message2
+            "5:17: "+createMessage("|"),
+            "9:25: "+createMessage("|"),
+            "11:15: "+createMessage("|"),
+            "24:20: "+createMessage("|"),
+            "33:25: "+createMessage("|"),
+            "46:25: "+createMessage("|"),
+            "51:16: "+createMessage("&"),
                 };
-
+        
         System.setProperty(
                 "testinputs.dir",
-                //"/media/Data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
-                "/home/developer/Daniil Yaroslavtsev/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+              //  "/media/B32C-8EF7/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
+                "/media/data/Work/sevntu.checkstyle/sevntu.checkstyle/src/testinputs/com/puppycrawl/tools/checkstyle/coding");
         //verify(checkConfig, getPath("coding" + File.separator + "InputAvoidNotShortCircuitOperatorsForBooleanCheck1.java"), expected);
         verify(checkConfig,
                 getPath("InputAvoidNotShortCircuitOperatorsForBooleanCheck1.java"), expected);
     }
 
+    public String createMessage(String literal){
+        return "Not short-circuit Operator '" + literal + "' used.";      
+    }
+    
 }
