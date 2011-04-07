e6471d9f986c0f450a81251066bc7e9616b35f27
Selden <selden@Selden-PC.(none)>
2011-04-07 03:49:39 +0300

    Full clean

--- src/testinputs/com/puppycrawl/tools/checkstyle/design/HideUtilityClassContructor3041574_1.java
+++
@@ -1,8 +0,0 @@
-package com.puppycrawl.tools.checkstyle.design;
-
-import java.io.Serializable;
-
-public abstract class HideUtilityClassContructor3041574_1 implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-}
