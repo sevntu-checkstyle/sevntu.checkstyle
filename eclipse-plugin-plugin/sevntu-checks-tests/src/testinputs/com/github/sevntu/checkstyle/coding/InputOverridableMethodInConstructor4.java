ceeebcf502e9f28a8692c0a648094dbc4745813d
Ivan Sopov <moradan228@gmail.com>
2011-05-28 09:47:59 +0300

    OverridableMethodInConstructorCheck was transferred to EPP

---
+++ eclipse-plugin-plugin/sevntu-checks-tests/src/testinputs/com/github/sevntu/checkstyle/coding/InputOverridableMethodInConstructor4.java
@@ -0,0 +1,69 @@
+package com.github.sevntu.checkstyle.coding;
+
+public class InputOverridableMethodInConstructor4 implements Cloneable {
+   private int a;
+   private SomeData someData = null;
+
+   public InputOverridableMethodInConstructor4(final int value, final SomeData prop){
+       setA(value);
+       setSomeData(prop);
+    }
+   
+   public Object clone(){
+      Object result = null;
+      try{
+         result = super.clone();
+      } catch (CloneNotSupportedException e){
+         e.printStackTrace();
+      }
+      return result;
+   }
+   
+   
+   public void setA(int a){
+      this.a = a;
+   }
+   
+   public int getA(){
+      return a;
+   }
+   
+   public void setSomeData(SomeData someData){
+      this.someData = someData;
+   }
+   
+   public SomeData getSomeData(){
+      return someData;
+   }
+      
+   
+   
+   public class SomeData implements Cloneable{
+       private int someInt;
+       
+       public SomeData(final int data){
+           someInt = data;
+        }
+       
+       public Object clone(){
+          Object result = null;
+          try{
+             result = super.clone();
+          } catch (CloneNotSupportedException e){
+             e.printStackTrace();
+          }
+          return result;
+       }
+
+       public void setSomeInt(int someInt){
+          this.someInt = someInt;
+       }
+
+       public int getSomeInt(){
+          return someInt;
+       }
+    }
+   
+   
+}
+
