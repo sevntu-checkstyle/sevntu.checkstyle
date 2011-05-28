4f4dd95597346651eec7eb37d4a421109114d5b0
Ivan Sopov <moradan228@gmail.com>
2011-05-28 13:12:52 +0300

    LineLengthWithIgnore transfered with tests

---
+++ eclipse-plugin-plugin/sevntu-checks-tests/src/test/java/com/github/sevntu/checkstyle/checks/sizes/LineLengthWithIgnoreCheckTest.java
@@ -0,0 +1,57 @@
+package com.github.sevntu.checkstyle.checks.sizes;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import java.io.File;
+import org.junit.Test;
+
+public class LineLengthWithIgnoreCheckTest extends BaseCheckTestSupport
+{
+	@Test
+    public void testSimple()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(LineLengthWithIgnoreCheck.class);
+        checkConfig.addAttribute("max", "80");
+        checkConfig.addAttribute("ignorePattern",  "^.*is OK.*regexp.*$");
+        final String[] expected = {
+            "18: Line is longer than 80 characters.",
+            "145: Line is longer than 80 characters.",
+        };
+       verify(checkConfig, getPath("sizes" + File.separator + "InputSimple.java"), expected);
+    }
+
+    @Test
+    public void testSimpleIgnore()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(LineLengthWithIgnoreCheck.class);
+        checkConfig.addAttribute("max", "40");
+        checkConfig.addAttribute("ignorePattern",  "^.*is OK.*regexp.*$");
+        final String[] expected = {
+            "1: Line is longer than 40 characters.",
+            "5: Line is longer than 40 characters.",
+            "6: Line is longer than 40 characters.",
+            "18: Line is longer than 40 characters.",
+            "101: Line is longer than 40 characters.",
+            "125: Line is longer than 40 characters.",
+            "128: Line is longer than 40 characters.",
+            "132: Line is longer than 40 characters.",
+            "145: Line is longer than 40 characters.",
+            "146: Line is longer than 40 characters.",
+            "148: Line is longer than 40 characters.",
+            "151: Line is longer than 40 characters.",
+            "152: Line is longer than 40 characters.",
+            "192: Line is longer than 40 characters.",
+            "200: Line is longer than 40 characters.",
+            "207: Line is longer than 40 characters.",
+        };
+        checkConfig.addAttribute("ignoreClass", "true");
+        checkConfig.addAttribute("ignoreConstructor", "true");
+        checkConfig.addAttribute("ignoreField", "true");
+        checkConfig.addAttribute("ignoreMethod", "true");
+        verify(checkConfig, getPath("sizes" + File.separator + "InputSimple.java"), expected);
+    }
+}
