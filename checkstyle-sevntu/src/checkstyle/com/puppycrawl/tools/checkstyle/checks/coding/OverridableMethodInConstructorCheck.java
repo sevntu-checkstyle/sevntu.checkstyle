1d3314dd5d4bdb3f4d698773fedab8bb0d932324
selden <selden@Selden-PC>
2011-06-14 23:11:01 +0300

    Changes in OverridableMethodInCtorCheck. Now it must work fine in plugin-plugin (XML data and config added).

--- checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/OverridableMethodInConstructorCheck.java
+++ checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/coding/OverridableMethodInConstructorCheck.java
@@ -1,20 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
-//checkstyle: Checks Java source code for adherence to a set of rules.
-//Copyright (C) 2001-2010  Oliver Burn
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2010  Oliver Burn
 //
-//This library is free software; you can redistribute it and/or
-//modify it under the terms of the GNU Lesser General Public
-//License as published by the Free Software Foundation; either
-//version 2.1 of the License, or (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-//This library is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//Lesser General Public License for more details.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
-//You should have received a copy of the GNU Lesser General Public
-//License along with this library; if not, write to the Free Software
-//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
@@ -24,7 +24,7 @@ import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.api.Check;
 
-/**<p> This check prevents calls of overridable methods in constructor body.
+/**<p> This check prevents calls of overridable methods from constructor body.
  * @author <a href="mailto:Daniil.Yaroslavtsev@gmail.com"> Daniil
  *         Yaroslavtsev</a>
  */
@@ -36,53 +36,57 @@ public class OverridableMethodInConstructorCheck extends Check {
     private final String mKey = "overridable.method.in.constructor";
 
     /**
-     * A list contains all names of operands, which are used in the current
-     * expression, which calculates with using "|", "&", "|=", "&=" operators.
+     * A list contains DetailAST nodes for all methods that called
+     * from C-tors in current class.
      * */
-    private LinkedList<DetailAST> mOverridableMethodsList = new LinkedList<DetailAST>();
-    
+    private LinkedList<DetailAST> mOverridableMethodsList =
+        new LinkedList<DetailAST>();
     /**
-     * A list contains all names of operands, which are used in the current
-     * expression, which calculates with using "|", "&", "|=", "&=" operators.
+     * A list of objects that contain a DetailAST node for each method is
+     * invoked in C-tor and related DetailAST node for the class that owns
+     * this C-tor.
      * */
-    private LinkedList<MethodCalledFromCtor> mMethodCalledInConstructorList = new LinkedList<MethodCalledFromCtor>();
+    private LinkedList<MethodCalledFromCtor> mMethodCalledInCtorList =
+        new LinkedList<MethodCalledFromCtor>();
 
     @Override
-    public final int[] getDefaultTokens() {
-        return new int[] { TokenTypes.METHOD_CALL, TokenTypes.METHOD_DEF };
+    public final int[] getDefaultTokens()
+    {
+        return new int[] {TokenTypes.METHOD_CALL, TokenTypes.METHOD_DEF};
     }
 
     @Override
-    public final void visitToken(final DetailAST aDetailAST) {
-        System.out.println(aDetailAST);
-
-        if (aDetailAST.getType() == TokenTypes.METHOD_CALL) { // for METHOD_CALL nodes
+    public final void visitToken(final DetailAST aDetailAST)
+    {
+     // for all METHOD_CALL nodes
+        if (aDetailAST.getType() == TokenTypes.METHOD_CALL) {
 
             if (aDetailAST.getFirstChild() != null
-                    && aDetailAST.getFirstChild().getType() != TokenTypes.DOT) {
+                    && aDetailAST.getFirstChild().getType() != TokenTypes.DOT)
+            {
 
                 DetailAST currentNode = aDetailAST;
 
                 while (currentNode != null
                         && currentNode.getType() != TokenTypes.METHOD_DEF
                         && currentNode.getType() != TokenTypes.CTOR_DEF
-                        && currentNode.getType() != TokenTypes.CLASS_DEF) {
+                        && currentNode.getType() != TokenTypes.CLASS_DEF)
+                {
                     currentNode = currentNode.getParent();
                 }
 
                 if (currentNode.getType() == TokenTypes.CTOR_DEF) {
-                    DetailAST curClass = currentNode.getParent().getParent();
-                    mMethodCalledInConstructorList
-                            .add(new MethodCalledFromCtor(aDetailAST,
-                                    currentNode, curClass));
+                    final DetailAST curClass = currentNode.getParent()
+                            .getParent();
+                    mMethodCalledInCtorList.add(new MethodCalledFromCtor(
+                            aDetailAST, curClass));
                 }
-
             }
         }
 
-        else { // for METHOD_DEF nodes
+        else { // for all METHOD_DEF nodes
 
-            DetailAST modifiers = aDetailAST
+            final DetailAST modifiers = aDetailAST
                     .findFirstToken(TokenTypes.MODIFIERS);
 
             boolean hasPrivateModifier = false;
@@ -107,103 +111,101 @@ public class OverridableMethodInConstructorCheck extends Check {
     }
 
     @Override
-    public void finishTree(DetailAST aRootAST) {
+    public final void finishTree(final DetailAST aRootAST)
+    {
 
-        //  System.out.println("Overridable Methods List: " + mOverridableMethodsList);
-        //  System.out.println();
+        for (MethodCalledFromCtor curMethod : mMethodCalledInCtorList) {
 
-        //  System.out.println("Methods are Called In C-tor: ");
-//        int o=0;
-//        for(MethodCalledFromCtor next: mMethodCalledInConstructorList){
-//            o++;
-//            System.out.println(
-//                    "Method " + o + ": Method node: " + next.methodNode.toString() + ", ctor node: " +
-//                    next.ctorNode.toString()  + ", classNode: " + next.classNode.toString());
-//        }
-
-        for (MethodCalledFromCtor curMethod : mMethodCalledInConstructorList) {
-
-            String curMethodName = curMethod.methodNode.findFirstToken(
+            final String curMethodName = curMethod.mMethodNode.findFirstToken(
                     TokenTypes.IDENT).getText();
 
             for (DetailAST curOverridableMethod : mOverridableMethodsList) {
 
-                String curOverridableMethodName = curOverridableMethod
+                final String curOverridableMethodName = curOverridableMethod
                         .findFirstToken(TokenTypes.IDENT).getText();
 
-                String a = curMethod.classNode.toString();
-                String b = getClass(curOverridableMethod).toString();
-                
+                final String a = curMethod.mClassNode.toString();
+                final String b = getClass(curOverridableMethod).toString();
+
                 if (curMethodName.equals(curOverridableMethodName)
-                        && a.equals(b)) {
-                    log(curMethod.methodNode, mKey, curMethodName);
-                    //System.out.println("Error!");
+                        && a.equals(b))
+                {
+                    log(curMethod.mMethodNode, mKey, curMethodName);
                 }
-
             }
-
         }
-
-//        for(DetailAST curNode: mOverridableMethodsList) {
-//            
-//            DetailAST currentNode = curNode;
-//            
-//            while (currentNode != null
-//                    && currentNode.getType() != TokenTypes.CLASS_DEF)
-//            {
-//                currentNode = currentNode.getParent();
-//            }
-//
-////            if(currentNode.getType() != null
-////                    && 
-////                    ){
-//
-//           // }
-//            
-//        }
-
     }
 
-    public DetailAST getClass(DetailAST aMethodNode) {
+
+    /**
+     * Method that returns a root CLASS_DEF DetailAST for the class that owns
+     * a method for aMethodNode METHOD_CALL node.
+     * @return a DetailAST node for the class that owns
+     * aMethodNode node.
+     * @param aMethodNode - a current method DetailAST.
+     * */
+    public final DetailAST getClass(final DetailAST aMethodNode)
+    {
         DetailAST result = null;
         DetailAST currentNode = aMethodNode;
 
         while (currentNode != null
-                && currentNode.getType() != TokenTypes.CLASS_DEF) {
+                && currentNode.getType() != TokenTypes.CLASS_DEF)
+        {
             currentNode = currentNode.getParent();
         }
 
         if (currentNode != null
-                && currentNode.getType() == TokenTypes.CLASS_DEF) {
+                && currentNode.getType() == TokenTypes.CLASS_DEF)
+        {
             result = currentNode;
         }
 
         return result;
     }
 
-    public class MethodCalledFromCtor {
+    /**
+     * Class, an object of which contains a DetailAST node for
+     * current method that called from C-tor and a DetailAST node for
+     * the class that owns that C-tor.
+     * */
+    public class MethodCalledFromCtor
+    {
 
-        protected DetailAST methodNode;
-        protected DetailAST ctorNode;
-        protected DetailAST classNode;
+        /**
+         * A DetailAST node for current method that called from C-tor.
+         * */
+        private DetailAST mMethodNode;
+        /**
+         * A CLASS_DEF DetailAST node for the class that owns the C-tor
+         * contains the call of method named methodNode.
+         * */
+        private DetailAST mClassNode;
 
-        MethodCalledFromCtor(DetailAST aMethodNode, DetailAST aCtorNode,
-                DetailAST aClassNode) {
-            this.methodNode = aMethodNode;
-            this.ctorNode = aCtorNode;
-            this.classNode = aClassNode;
+        /**
+         * C-tor of "MethodCalledFromCtor" object.
+         * @param aMethodNode DetailAST node for current method that called from
+         * C-tor.
+         * @param aClassNode A CLASS_DEF DetailAST node for the class that owns
+         * the C-tor contains the call of method named methodNode.
+         * */
+        MethodCalledFromCtor(final DetailAST aMethodNode,
+                final DetailAST aClassNode)
+        {
+            this.mMethodNode = aMethodNode;
+            this.mClassNode = aClassNode;
         }
 
     }
 
     /**
      * Gets all the children one level below on the current top node.
-     * 
      * @param aNode - current parent node.
      * @return an array of children one level below on the current parent node
      *         aNode.
      */
-    public final LinkedList<DetailAST> getChildren(final DetailAST aNode) {
+    public final LinkedList<DetailAST> getChildren(final DetailAST aNode)
+    {
         final LinkedList<DetailAST> result = new LinkedList<DetailAST>();
 
         DetailAST currNode = aNode.getFirstChild();
