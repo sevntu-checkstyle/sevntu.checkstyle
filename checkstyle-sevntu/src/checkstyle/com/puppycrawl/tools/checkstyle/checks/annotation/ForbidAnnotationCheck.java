142e5f5fe7e38f49bd19d61001931685d528f042
Hidoyatov Victor <hidoyatov.v.i@gmail.com>
2011-07-21 15:19:29 +0300

    ForbidAnnotationCheck

--- checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/annotation/ForbidAnnotationCheck.java
+++ checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/annotation/ForbidAnnotationCheck.java
@@ -4,33 +4,38 @@ import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 /**
  * 
  * @author <a href="mailto:hidoyatov.v.i@gmail.com">Hidoyatov Victor</a> 
  */
 /**
- * Forbid specific annotation of variable,methods,class,package and other. 
- * If you want to forbid use 'XXX' annotation with methods and class, you mast write: 
- * <module name="ForbidAnnotation"> 
- * <property name="annotation" value="XXX"/> 
- * <property name="target" value="METHOD_DEF,CLASS_DEF"/> </module>
+ * Forbid specific annotation of variable,methods,class,package and other. If
+ * you want to forbid use 'XXX' annotation with methods and class, you mast
+ * write: <module name="ForbidAnnotation"> <property name="annotation"
+ * value="XXX"/> <property name="target" value="METHOD_DEF,CLASS_DEF"/>
+ * </module>
  */
 
 public class ForbidAnnotationCheck extends Check {
-	private String[] annotationName;
-	private int[] annotationTarget;
+	private Set<String> annotationName = new HashSet<String>();
+	private Set<Integer> annotationTarget = new HashSet<Integer>();
 
-	public void setAnnotation(final String[] aName) {
-		if (aName != null) {
-			annotationName = aName;
+	public void setAnnotation(final String[] aNames) {
+		if (aNames != null) {
+			for(String aName:aNames){
+				annotationName.add(aName);
+			}
 		}
 	}
 
-	public void setTarget(String[] aTarget) {
-		if (aTarget != null) {
-			annotationTarget = new int[aTarget.length];
-			for (int i = 0; i < aTarget.length; i++) {
-				annotationTarget[i] = TokenTypes.getTokenId(aTarget[i]);
+	public void setTarget(String[] aTargets) {
+		if (aTargets != null) {
+			for (String aTarget: aTargets) {
+				annotationTarget.add(TokenTypes.getTokenId(aTarget));
 			}
 		}
 	}
@@ -40,31 +45,21 @@ public class ForbidAnnotationCheck extends Check {
 		return new int[] { TokenTypes.ANNOTATION };
 	}
 
-	public void visitToken(DetailAST aAST) {
-		int currentAnnotetion = 0;
-		String currentTarget = "";
-		boolean isValidAnnotation = false;
-		boolean isValidTarget = false;
-
-		int type = aAST.getParent().getParent().getType();
-		for (int i = 0; i < annotationTarget.length; i++) {
-			if (type == annotationTarget[i]) {
-				currentTarget = aAST.getParent().getParent().getText();
-				isValidTarget = true;
-			}
+	public void visitToken(DetailAST aAnnotation) {
+		String aAnnotationName = aAnnotation.findFirstToken(TokenTypes.IDENT).getText();
+		int aTargetType = aAnnotation.getParent().getParent().getType();
+		if (isAnnotation(aAnnotationName) && isForbidden(aTargetType)) {
+			String currentTarget = aAnnotation.getParent().getParent().getText();
+			log(aAnnotation.getLineNo(), "annotation.incorrect.target", currentTarget,
+					aAnnotationName);
 		}
-
-		String name = aAST.findFirstToken(TokenTypes.IDENT).getText();
-		for (int i = 0; i < annotationName.length; i++) {
-			if (name.equals(annotationName[i])) {
-				currentAnnotetion = i;
-				isValidAnnotation = true;
-			}
-		}
-
-		if (isValidAnnotation && isValidTarget) {
-			log(aAST.getLineNo(), "annotation.incorrect.target", currentTarget,
-					annotationName[currentAnnotetion]);
-		}
+	}
+	
+	private boolean isAnnotation(String aAnnotationName){
+			return aAnnotationName != null && annotationName.contains(aAnnotationName);
+	}
+	
+	private boolean isForbidden(int aTargetType){
+		return annotationTarget.contains(aTargetType);
 	}
 }
