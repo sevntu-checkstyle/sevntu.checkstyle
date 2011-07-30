20aa48b5a089c784d3ab96071b8222b4e5c0cfc9
Ruslan Diachenko <rd.ryly@gmail.com>
2011-07-30 23:07:50 +0300

    RegEx validation for variables was added. Tests were updated.

--- checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheckTest.java
+++ checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheckTest.java
@@ -9,10 +9,10 @@ import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
 
 public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupport {
 	@Test
-	public void testDistance() throws Exception {
+	public void testGeneralLogic() throws Exception {
 		final DefaultConfiguration checkConfig = createCheckConfig(VariableDeclarationUsageDistanceCheck.class);
-//		checkConfig.addAttribute("distance", "0");
-//		checkConfig.addAttribute("ignoreVariablePattern", "temp");
+		checkConfig.addAttribute("distance", "0");
+		checkConfig.addAttribute("regExp", "");
 		final String[] expected = {
 				"11: Declaration of variable 'b' should be here.",
 				"11: Declaration of variable 'd' should be here.",
@@ -35,6 +35,40 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 				"105: Declaration of variable 'block' should be here.",
 				"105: Declaration of variable 'blockNumWithSimilarVar' should be here.",
 				"105: Declaration of variable 'dist' should be here.",
+				"105: Declaration of variable 'index' should be here.",
+				"125: Declaration of variable 'res' should be here.",
+		};
+		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"), expected);
+	}
+	
+	@Test
+	public void testDistance() throws Exception {
+		final DefaultConfiguration checkConfig = createCheckConfig(VariableDeclarationUsageDistanceCheck.class);
+		checkConfig.addAttribute("distance", "2");
+		checkConfig.addAttribute("regExp", "");
+		final String[] expected = {
+				"33: Declaration of variable 'a' should be here.",
+				"65: Declaration of variable 'count' should be here.",
+				"76: Declaration of variable 'count' should be here.",
+				"105: Declaration of variable 'blockNumWithSimilarVar' should be here.",
+				"105: Declaration of variable 'dist' should be here.",
+		};
+		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"), expected);
+	}
+	
+	@Test
+	public void testVariableRegExp() throws Exception {
+		final DefaultConfiguration checkConfig = createCheckConfig(VariableDeclarationUsageDistanceCheck.class);
+		checkConfig.addAttribute("distance", "0");
+		checkConfig.addAttribute("regExp", "a|b|c|d|block|dist");
+		final String[] expected = {
+				"39: Declaration of variable 'temp' should be here.",
+				"45: Declaration of variable 'temp' should be here.",
+				"48: Declaration of variable 'result' should be here.",
+				"48: Declaration of variable 'str' should be here.",
+				"65: Declaration of variable 'count' should be here.",
+				"76: Declaration of variable 'count' should be here.",
+				"105: Declaration of variable 'blockNumWithSimilarVar' should be here.",
 				"105: Declaration of variable 'index' should be here.",
 				"125: Declaration of variable 'res' should be here.",
 		};
