7b3b1ef96ce69af7ab75c25a350bc3f9513a0369
Ruslan Diachenko <rd.ryly@gmail.com>
2011-08-03 00:33:16 +0300

    Algorithm was overwrote. Test cases were updated.

--- checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheck.java
+++ checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheck.java
@@ -1,9 +1,7 @@
 package com.puppycrawl.tools.checkstyle.checks.design;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,212 +13,151 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class VariableDeclarationUsageDistanceCheck extends Check {
 
-	private List<DetailAST> variableDefAndExprList;
+	private int allowedDistance;
 
-	private List<DetailAST> variableDefList;
+	private String ignoreVariables;
 
-	private List<Integer> variableDefAndExprLineList;
+	private boolean ignoreSimpleDeclaration;
 
-	private int distance;
+	private int errorLine;
 
-	private String regExp;
+	private boolean variableMeet;
 
-	public int getDistance() {
-		return distance;
-	}
-	
-	public void setDistance(int distance) {
-		this.distance = distance;
+	public int getAllowedDistance() {
+		return allowedDistance;
 	}
 
-	public String getRegExp() {
-		return regExp;
+	public void setAllowedDistance(int allowedDistance) {
+		this.allowedDistance = allowedDistance;
 	}
-	
-	public void setRegExp(String regExp) {
-		this.regExp = regExp;
+
+	public String getIgnoreVariables() {
+		if (ignoreVariables == null) {
+			ignoreVariables = "";
+		}
+		return ignoreVariables;
+	}
+
+	public void setIgnoreVariables(String ignoreVariables) {
+		this.ignoreVariables = ignoreVariables;
+	}
+
+	public boolean isIgnoreSimpleDeclaration() {
+		return ignoreSimpleDeclaration;
+	}
+
+	public void setIgnoreSimpleDeclaration(boolean ignoreSimpleDeclaration) {
+		this.ignoreSimpleDeclaration = ignoreSimpleDeclaration;
 	}
 
 	@Override
 	public int[] getDefaultTokens() {
-		return new int[] { TokenTypes.STATIC_INIT, TokenTypes.CTOR_DEF, TokenTypes.METHOD_DEF,
-				TokenTypes.VARIABLE_DEF, TokenTypes.EXPR, TokenTypes.LITERAL_IF, TokenTypes.LITERAL_FOR,
-				TokenTypes.LITERAL_WHILE, TokenTypes.LITERAL_SWITCH, };
+		return new int[] { TokenTypes.VARIABLE_DEF, };
 	}
 
 	@Override
 	public void visitToken(DetailAST aAST) {
+		variableMeet = false;
 		int parentType = aAST.getParent().getType();
-		switch (aAST.getType()) {
-		case TokenTypes.VARIABLE_DEF:
-			if (parentType != TokenTypes.OBJBLOCK && checkExprAndVariable(aAST)) {
-				if (!variableDefList.contains(aAST)) {
-					variableDefList.add(aAST);
+		DetailAST nextSibling = aAST.getNextSibling();
+		if (parentType != TokenTypes.OBJBLOCK && nextSibling != null && nextSibling.getType() == TokenTypes.SEMI) {
+			DetailAST variable = aAST.findFirstToken(TokenTypes.IDENT);
+			if (!isVariableMatchesPattern(variable.getText())) {
+				int dist = calculateDistance(nextSibling, variable);
+				if (variableMeet) {
+					dist++;
+					if (!isDistanceAllowed(dist) && dist > 0) {
+						// TODO: log error line
+						System.out.println("dist = " + dist + "; error = " + errorLine);
+					}
 				}
-				variableDefAndExprList.add(aAST);
-				variableDefAndExprLineList.add(aAST.getLineNo());
 			}
-			break;
-		case TokenTypes.EXPR:
-			if (parentType == TokenTypes.OBJBLOCK || !checkExprAndVariable(aAST)) {
+		}
+	}
+
+	private int calculateDistance(DetailAST ast, DetailAST variable) {
+		int dist = 0;
+		boolean errorLineWasFound = false;
+		boolean variableFirstMeet = false;
+		boolean isExpInBracket = false;
+		DetailAST nextSibling = ast;
+		List<DetailAST> exprWithVariableList = new ArrayList<DetailAST>();
+		while (nextSibling != null && nextSibling.getType() != TokenTypes.RCURLY) {
+			switch (nextSibling.getType()) {
+			case TokenTypes.VARIABLE_DEF:
+				if (nextSibling.getLastChild().getType() == TokenTypes.IDENT && isIgnoreSimpleDeclaration()) {
+					break;
+				}
+			case TokenTypes.LPAREN:
+				isExpInBracket = true;
 				break;
-			}
-		case TokenTypes.LITERAL_IF:
-		case TokenTypes.LITERAL_FOR:
-		case TokenTypes.LITERAL_WHILE:
-		case TokenTypes.LITERAL_SWITCH:
-			variableDefAndExprList.add(aAST);
-			variableDefAndExprLineList.add(aAST.getLineNo());
-			break;
-		default:
-			if (parentType == TokenTypes.OBJBLOCK) {
-				variableDefAndExprList = new ArrayList<DetailAST>();
-				variableDefList = new ArrayList<DetailAST>();
-				variableDefAndExprLineList = new ArrayList<Integer>();
-			}
-			break;
-		}
-	}
-
-	@Override
-	public void leaveToken(DetailAST aAST) {
-		if (aAST.getType() == TokenTypes.STATIC_INIT || aAST.getType() == TokenTypes.CTOR_DEF
-				|| aAST.getType() == TokenTypes.METHOD_DEF) {
-
-			for (int i = 0; i < variableDefList.size(); i++) {
-				DetailAST varDef = variableDefList.get(i);
-				DetailAST variableIdent = varDef.findFirstToken(TokenTypes.IDENT);
-				
-				if (isVariableMatchesPattern(variableIdent.getText())) {
-					continue;
-				}
-
-				boolean variableFirstMeet = false;
-				Map<Integer, Boolean> blocksOfVariableMeet = new HashMap<Integer, Boolean>();
-				Map<Integer, Integer> blocksOfVariableDistance = new HashMap<Integer, Integer>();
-				List<Integer> blocks = new ArrayList<Integer>();
-				List<Integer> varNestingList = new ArrayList<Integer>();
-				int varDefIndex = 0;
-
-				for (int j = 0; j < variableDefAndExprList.size(); j++) {
-					int key = variableDefAndExprList.get(j).getParent().getLineNo();
-					if (!blocksOfVariableMeet.containsKey(key)) {
-						blocks.add(key);
-						blocksOfVariableMeet.put(key, false);
-						blocksOfVariableDistance.put(key, 0);
-						varNestingList.add(getVariableNesting(aAST, variableDefAndExprList.get(j)));
-					}
-					if (!variableFirstMeet && variableDefAndExprList.get(j).getLineNo() == varDef.getLineNo()) {
+			case TokenTypes.RPAREN:
+				isExpInBracket = false;
+				break;
+			default:
+				if (nextSibling.getFirstChild() != null) {
+					if (isASTContainsElement(nextSibling, variable)) {
+						exprWithVariableList.add(nextSibling);
+						if (!errorLineWasFound) {
+							errorLine = nextSibling.getLineNo();
+							errorLineWasFound = true;
+						}
+						variableMeet = true;
 						variableFirstMeet = true;
-						varDefIndex = j;
-						continue;
-					}
-					if (variableFirstMeet) {
-						int currentDistance = blocksOfVariableDistance.get(key);
-						if (!blocksOfVariableMeet.get(key) && !blocksOfVariableMeet.containsValue(true)) {
-							blocksOfVariableDistance.put(key, ++currentDistance);
-						}
-						if (isExprContainsVariable(variableDefAndExprList.get(j), variableIdent)) {
-							blocksOfVariableMeet.put(key, true);
+					} else {
+						if (!variableFirstMeet && !isExpInBracket) {
+							dist++;
 						}
 					}
-				}
-
-				if (!blocksOfVariableMeet.containsValue(true)) {
-					continue;
-				}
-
-				int rootVarNesting = varNestingList.get(0);
-				int blockNumWithSimilarVar = 0;
-				for (int k = 0; k < blocks.size(); k++) {
-					int key = blocks.get(k);
-					if (varNestingList.get(k) - rootVarNesting == 1 && blocksOfVariableMeet.get(key)) {
-						blockNumWithSimilarVar++;
-					}
-				}
-
-				int dist = 0;
-				int index = 0;
-				int block = blocks.get(index);
-
-				if (blockNumWithSimilarVar <= 1) {
-					do { // If variable is only in one internal block
-						dist += blocksOfVariableDistance.get(block);
-						if (blocksOfVariableMeet.get(block)) {
-							break;
-						}
-						index++;
-						block = blocks.get(index);
-					} while (index < blocks.size());
-				} else {
-					// If variable is in more then one internal block
-					while (!blocksOfVariableMeet.get(block) && index < blocks.size()) {
-						dist += blocksOfVariableDistance.get(block);
-						index++;
-						block = blocks.get(index);
-					}
-				}
-				if (!checkDistance(dist)) {
-					log(variableDefAndExprLineList.get(varDefIndex + dist), "variable.declaration.usage.distance",
-							variableIdent.getText());
 				}
 			}
+			nextSibling = nextSibling.getNextSibling();
 		}
+
+		if (exprWithVariableList.size() != 0) {
+			DetailAST blockWithVariable = exprWithVariableList.get(0);
+
+			if (exprWithVariableList.size() == 1) {
+				if (blockWithVariable.getType() != TokenTypes.VARIABLE_DEF
+						&& blockWithVariable.getType() != TokenTypes.EXPR) {
+					dist += calculateDistance(blockWithVariable.getFirstChild(), variable);
+				}
+			}
+		} else {
+			if (!variableFirstMeet) {
+				dist = 0;
+			}
+		}
+		return dist;
 	}
 
-	private boolean isExprContainsVariable(DetailAST expr, DetailAST varIdent) {
-		int lastExprLine;
-		if (expr.getNextSibling() != null) {
-			lastExprLine = expr.getNextSibling().getLineNo();
-		} else {
-			lastExprLine = expr.getLineNo();
-		}
-		boolean isExprContainsVar = false;
-		ASTEnumeration astList = expr.findAllPartial(varIdent);
+	private boolean isASTContainsElement(DetailAST ast, DetailAST element) {
+		boolean isASTContainsElement = false;
+		ASTEnumeration astList = ast.findAllPartial(element);
 		while (astList.hasMoreNodes()) {
 			DetailAST astElement = (DetailAST) astList.nextNode();
-			if (astElement.getText().equals(varIdent.getText()) && astElement.getLineNo() <= lastExprLine) {
-				isExprContainsVar = true;
+			DetailAST astElementParent = astElement.getParent();
+			while (astElementParent != null) {
+				if (astElementParent.equals(ast) && astElementParent.getLineNo() == ast.getLineNo()) {
+					isASTContainsElement = true;
+					break;
+				}
+				astElementParent = astElementParent.getParent();
 			}
 		}
-		return isExprContainsVar;
+		return isASTContainsElement;
 	}
 
-	private int getVariableNesting(DetailAST rootAST, DetailAST nestedAST) {
-		int varNesting = -1;
-		DetailAST parent = nestedAST.getParent();
-		while (!rootAST.equals(parent)) {
-			varNesting++;
-			parent = parent.getParent();
-		}
-		return varNesting;
-	}
-
-	private boolean checkExprAndVariable(DetailAST exprOrVar) {
-		boolean isPassedCheck = true;
-		DetailAST parent = exprOrVar.getParent();
-		while (parent.getType() != TokenTypes.OBJBLOCK) {
-			if (parent.getType() == TokenTypes.LITERAL_IF || parent.getType() == TokenTypes.LITERAL_FOR
-					|| parent.getType() == TokenTypes.LITERAL_WHILE || parent.getType() == TokenTypes.LITERAL_SWITCH
-					|| parent.getType() == TokenTypes.VARIABLE_DEF || parent.getType() == TokenTypes.EXPR) {
-				isPassedCheck = false;
-				break;
-			}
-			parent = parent.getParent();
-		}
-		return isPassedCheck;
-	}
-
-	private boolean checkDistance(int distance) {
+	private boolean isDistanceAllowed(int distance) {
 		boolean isPassed = true;
-		if (distance > getDistance()) {
+		if (distance > getAllowedDistance()) {
 			isPassed = false;
 		}
 		return isPassed;
 	}
-	
+
 	private boolean isVariableMatchesPattern(String variable) {
-		Pattern pattern = Pattern.compile(getRegExp());
+		Pattern pattern = Pattern.compile(getIgnoreVariables());
 		Matcher matcher = pattern.matcher(variable);
 		return matcher.matches();
 	}
