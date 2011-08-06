2a807d71eb9f452a6c165e176fe510d6f611ff1d
Ruslan Diachenko <rd.ryly@gmail.com>
2011-08-06 17:58:27 +0300

    Check was modified. Documentation was updated.

--- checkstyle-sevntu/src/testinputs/com/puppycrawl/tools/checkstyle/design/InputVariableDeclarationUsageDistanceCheck.java
+++ checkstyle-sevntu/src/testinputs/com/puppycrawl/tools/checkstyle/design/InputVariableDeclarationUsageDistanceCheck.java
@@ -8,16 +8,16 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		int b;
 		int d;
 		{
-			d = ++b; // DECLARATION SHOULD BE HERE
+			d = ++b;
 		}
 	}
-	
+
 	static {
 		int c;
 		int a = 3;
 		int b = 2;
-		{ // DECLARATION SHOULD BE HERE
-			a = a + b; // DECLARATION SHOULD BE HERE
+		{
+			a = a + b;
 			c = b;
 		}
 		{
@@ -29,25 +29,25 @@ public class InputVariableDeclarationUsageDistanceCheck {
 	static {
 		int a = -1;
 		int b = 2;
-		b++; // DECLARATION SHOULD BE HERE
+		b++;
 		int c = --b;
-		a = b; // DECLARATION SHOULD BE HERE
+		a = b; // DECLARATION OF VARIABLE 'a' SHOULD BE HERE (distance = 2)
 	}
 
 	public InputVariableDeclarationUsageDistanceCheck(int test1) {
 		int temp = -1;
 		this.test1 = test1;
-		temp = test1; // DECLARATION SHOULD BE HERE
+		temp = test1; // DECLARATION OF VARIABLE 'temp' SHOULD BE HERE (distance = 2)
 	}
 
 	public boolean testMethod() {
 		int temp = 7;
 		new InputVariableDeclarationUsageDistanceCheck(2);
-		InputVariableDeclarationUsageDistanceCheck(temp); // DECLARATION SHOULD BE HERE
+		InputVariableDeclarationUsageDistanceCheck(temp); // DECLARATION OF VARIABLE 'temp' SHOULD BE HERE (distance = 2)
 		boolean result = false;
 		String str = "";
-		if (test1 > 1) { // DECLARATION SHOULD BE HERE
-			str = "123"; // DECLARATION SHOULD BE HERE
+		if (test1 > 1) {
+			str = "123";
 			result = true;
 		}
 		return result;
@@ -58,12 +58,12 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		int a = 3;
 		int b = 2;
 		{
-			a = a  // DECLARATION SHOULD BE HERE
-					+ b 
-					- 5 
-					+ 2 
+			a = a
+					+ b
+					- 5
+					+ 2
 					* a;
-			count = b; // DECLARATION SHOULD BE HERE
+			count = b; // DECLARATION OF VARIABLE 'count' SHOULD BE HERE (distance = 2)
 		}
 	}
 
@@ -71,44 +71,44 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		int count;
 		int a = 3;
 		int b = 3;
-		a = a + b; // DECLARATION SHOULD BE HERE
+		a = a + b;
 		b = a + a;
 		testMethod2();
-		count = b; // DECLARATION SHOULD BE HERE
+		count = b; // DECLARATION OF VARIABLE 'count' SHOULD BE HERE (distance = 4)
 	}
-	
+
 	public void testMethod4(int arg) {
 		int d;
 		for (int i = 0; i < 10; i++) {
-			d++; // DECLARATION SHOULD BE HERE
+			d++;
 			if (i > 5) {
 				d += arg;
 			}
 		}
-		
-		String ar[] = {"1", "2"};
-		for (String st : ar) { // DECLARATION SHOULD BE HERE
+
+		String ar[] = { "1", "2" };
+		for (String st : ar) {
 			System.out.println(st);
 		}
 	}
-	
+
 	public void testMethod5() {
 		int arg = 7;
 		boolean b = true;
 		boolean bb = false;
-		if (b) // DECLARATION SHOULD BE HERE
-			if (!bb) // DECLARATION SHOULD BE HERE
+		if (b)
+			if (!bb)
 				b = false;
-		testMethod4(arg); // DECLARATION SHOULD BE HERE
+		testMethod4(arg); // DECLARATION OF VARIABLE 'arg' SHOULD BE HERE (distance = 2)
 	}
-	
+
 	public void testMethod6() {
 		int blockNumWithSimilarVar = 3;
 		int dist = 0;
 		int index = 0;
 		int block = 0;
-		
-		if (blockNumWithSimilarVar <= 1) { // DECLARATION SHOULD BE HERE
+
+		if (blockNumWithSimilarVar <= 1) {
 			do {
 				dist++;
 				if (block > 4) {
@@ -125,17 +125,17 @@ public class InputVariableDeclarationUsageDistanceCheck {
 			}
 		}
 	}
-	
+
 	public boolean testMethod7(int a) {
 		boolean res;
-		switch(a) { // DECLARATION SHOULD BE HERE
+		switch (a) {
 		case 1:
 			res = true;
 			break;
 		default:
 			res = false;
 		}
-		return res;	
+		return res;
 	}
 
 	public void testMethod8() {
@@ -143,32 +143,32 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		int c;
 		int m;
 		int n;
-		{ // DECLARATION SHOULD BE HERE
-			c++; // DECLARATION SHOULD BE HERE
+		{
+			c++;
 			b++;
 		}
 		{
-			n++; // DECLARATION SHOULD BE HERE
-			m++; // DECLARATION SHOULD BE HERE
+			n++; // DECLARATION OF VARIABLE 'n' SHOULD BE HERE (distance = 2)
+			m++; // DECLARATION OF VARIABLE 'm' SHOULD BE HERE (distance = 3)
 			b++;
 		}
 	}
-	
+
 	public void testMethod9() {
 		boolean result = false;
 		boolean b1 = true;
 		boolean b2 = false;
-		if (b1) { // DECLARATION SHOULD BE HERE
-			if (!b2) { // DECLARATION SHOULD BE HERE
+		if (b1) {
+			if (!b2) {
 				result = true;
-		    }
-		    result = true;
+			}
+			result = true;
 		}
 	}
-	
+
 	public boolean testMethod10() {
 		boolean result;
-		try { // DECLARATION SHOULD BE HERE
+		try {
 			result = true;
 		} catch (IOException e) {
 			result = false;
@@ -177,17 +177,18 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		}
 		return result;
 	}
-	
+
 	public void testMethod11() {
 		int a = 0;
 		int b = 10;
 		boolean result;
 		try {
-			b--; // DECLARATION SHOULD BE HERE
+			b--;
 		} catch (IOException e) {
-			result = false; // DECLARATION SHOULD BE HERE
+			b++;
+			result = false; // DECLARATION OF VARIABLE 'result' SHOULD BE HERE (distance = 2)
 		} finally {
-			a++; // DECLARATION SHOULD BE HERE
+			a++;
 		}
 	}
 
@@ -196,23 +197,23 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		boolean b3 = true;
 		boolean b1 = true;
 		boolean b2 = false;
-		if (b1) { // DECLARATION SHOULD BE HERE
-			if (b3) { // DECLARATION SHOULD BE HERE
-				if (!b2) { // DECLARATION SHOULD BE HERE
+		if (b1) {
+			if (b3) {
+				if (!b2) {
 					result = true;
 				}
 				result = true;
 			}
 		}
 	}
-	
+
 	public void testMethod13() {
 		int i = 9;
 		int j = 6;
-		int g = i + 8; // DECLARATION SHOULD BE HERE
-		int k = j + 10; // DECLARATION SHOULD BE HERE
+		int g = i + 8;
+		int k = j + 10;
 	}
-	
+
 	public void testMethod14() {
 		Session s = openSession();
 		Transaction t = s.beginTransaction();
@@ -220,24 +221,50 @@ public class InputVariableDeclarationUsageDistanceCheck {
 		E d1 = new E();
 		C1 c = new C1();
 		E d2 = new E();
-		a.setForward(d1); // DECLARATION SHOULD BE HERE
+		a.setForward(d1);
 		d1.setReverse(a);
-		c.setForward(d2); // DECLARATION SHOULD BE HERE
+		c.setForward(d2); // DECLARATION OF VARIABLE 'c' SHOULD BE HERE (distance = 3)
+						  // DECLARATION OF VARIABLE 'd2' SHOULD BE HERE (distance = 3)
 		d2.setReverse(c);
 		Serializable aid = s.save(a);
 		Serializable d2id = s.save(d2);
-		t.commit(); // DECLARATION SHOULD BE HERE
+		t.commit(); // DECLARATION OF VARIABLE 't' SHOULD BE HERE (distance = 5)
 		s.close();
 	}
-	
-	public void testMethod15() {
-		final int runnable = 0;
-		int i = 0;
-		int j;
-        EventQueue.invokeLater(new Runnable() {
-        	public void run() {
-        		runnable++; // DECLARATION SHOULD BE HERE
-        	}
-        });
+
+	public boolean isCheckBoxEnabled(TreePath path) {
+		DataLabelModel model = (DataLabelModel) getModel();
+		if (recursiveState) {
+			for (int index = 0; index < path.getPathCount(); ++index) {
+				int nodeIndex = model.getNodeIndex(path.getPathComponent(index));
+				if (disabled.contains(nodeIndex)) {
+					return false;
+				}
+			}
+		} else {
+			int nodeIndex = model.getNodeIndex(path.getLastPathComponent());
+			if (disabled.contains(nodeIndex)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public Object readObject(IObjectInputStream in) throws IOException {
+		SimpleDay startDay = new SimpleDay(in.readInt());
+		SimpleDay endDay = new SimpleDay(in.readInt());
+		return new SimplePeriod(startDay, endDay);
+	}
+
+	public int[] getSelectedIndices() {
+		int[] selected = new int[paths.length];
+		DataLabelModel model = (DataLabelModel) getModel();
+		int a = 0;
+		a++;
+		for (int index = 0; index < paths.length; ++index) {
+			selected[index] = model.getNodeIndex(paths[index].getLastPathComponent()); // DECLARATION OF VARIABLE 'selected' SHOULD BE HERE (distance = 2)
+																					   // DECLARATION OF VARIABLE 'model' SHOULD BE HERE (distance = 2)
+		}
+		return selected;
 	}
 }
