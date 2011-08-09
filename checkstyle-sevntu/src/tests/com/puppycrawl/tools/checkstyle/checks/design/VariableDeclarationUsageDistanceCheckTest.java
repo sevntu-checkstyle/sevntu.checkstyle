56cb5163d72a752820b37e9d7084569512e225d5
Ruslan Diachenko <rd.ryly@gmail.com>
2011-08-09 20:08:05 +0300

    'ignoreFinal' option was provided. Bug with 'validateBetweenScopes' option was fixed. Tests and test cases were updated.

--- checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheckTest.java
+++ checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/design/VariableDeclarationUsageDistanceCheckTest.java
@@ -15,6 +15,7 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 		checkConfig.addAttribute("allowedDistance", "1");
 		checkConfig.addAttribute("ignoreVariablePattern", "");
 		checkConfig.addAttribute("validateBetweenScopes", "true");
+		checkConfig.addAttribute("ignoreFinal", "false");
 		final String[] expected = {
 				"30: Distance between variable 'a' declaration and its first usage is 2, but allowed 1.",
 				"38: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
@@ -33,6 +34,9 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 				"287: Distance between variable 'sw' declaration and its first usage is 2, but allowed 1.",
 				"300: Distance between variable 'wh' declaration and its first usage is 2, but allowed 1.",
 				"329: Distance between variable 'logLevel' declaration and its first usage is 2, but allowed 1.",
+				"343: Distance between variable 'green' declaration and its first usage is 2, but allowed 1.",
+                "344: Distance between variable 'blue' declaration and its first usage is 3, but allowed 1.",
+                "367: Distance between variable 'intervalMs' declaration and its first usage is 2, but allowed 1.",
 		};
 		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"),
 				expected);
@@ -44,6 +48,7 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 		checkConfig.addAttribute("allowedDistance", "3");
 		checkConfig.addAttribute("ignoreVariablePattern", "");
 		checkConfig.addAttribute("validateBetweenScopes", "true");
+		checkConfig.addAttribute("ignoreFinal", "false");
 		final String[] expected = {
 				"71: Distance between variable 'count' declaration and its first usage is 4, but allowed 3.",
 				"219: Distance between variable 't' declaration and its first usage is 5, but allowed 3.",
@@ -57,6 +62,7 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 		checkConfig.addAttribute("allowedDistance", "1");
 		checkConfig.addAttribute("ignoreVariablePattern", "a|b|c|d|block|dist|t|m");
 		checkConfig.addAttribute("validateBetweenScopes", "true");
+		checkConfig.addAttribute("ignoreFinal", "false");
 		final String[] expected = {
 				"38: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
 				"44: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
@@ -71,6 +77,9 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 				"287: Distance between variable 'sw' declaration and its first usage is 2, but allowed 1.",
 				"300: Distance between variable 'wh' declaration and its first usage is 2, but allowed 1.",
 				"329: Distance between variable 'logLevel' declaration and its first usage is 2, but allowed 1.",
+				"343: Distance between variable 'green' declaration and its first usage is 2, but allowed 1.",
+                "344: Distance between variable 'blue' declaration and its first usage is 3, but allowed 1.",
+                "367: Distance between variable 'intervalMs' declaration and its first usage is 2, but allowed 1.",
 		};
 		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"), expected);
 	}
@@ -81,6 +90,7 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 		checkConfig.addAttribute("allowedDistance", "1");
 		checkConfig.addAttribute("ignoreVariablePattern", "");
 		checkConfig.addAttribute("validateBetweenScopes", "false");
+		checkConfig.addAttribute("ignoreFinal", "false");
 		final String[] expected = {
 				"30: Distance between variable 'a' declaration and its first usage is 2, but allowed 1.",
 				"38: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
@@ -90,6 +100,41 @@ public class VariableDeclarationUsageDistanceCheckTest extends BaseCheckTestSupp
 				"219: Distance between variable 't' declaration and its first usage is 5, but allowed 1.",
 				"222: Distance between variable 'c' declaration and its first usage is 3, but allowed 1.",
 				"223: Distance between variable 'd2' declaration and its first usage is 3, but allowed 1.",
+				"300: Distance between variable 'wh' declaration and its first usage is 2, but allowed 1.",
+				"343: Distance between variable 'green' declaration and its first usage is 2, but allowed 1.",
+                "344: Distance between variable 'blue' declaration and its first usage is 3, but allowed 1.",
+                "367: Distance between variable 'intervalMs' declaration and its first usage is 2, but allowed 1.",
+		};
+		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"),
+				expected);
+	}
+	
+	@Test
+	public void testIgnoreFinalOption() throws Exception {
+		final DefaultConfiguration checkConfig = createCheckConfig(VariableDeclarationUsageDistanceCheck.class);
+		checkConfig.addAttribute("allowedDistance", "1");
+		checkConfig.addAttribute("ignoreVariablePattern", "");
+		checkConfig.addAttribute("validateBetweenScopes", "true");
+		checkConfig.addAttribute("ignoreFinal", "true");
+		final String[] expected = {
+				"30: Distance between variable 'a' declaration and its first usage is 2, but allowed 1.",
+				"38: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
+				"44: Distance between variable 'temp' declaration and its first usage is 2, but allowed 1.",
+				"57: Distance between variable 'count' declaration and its first usage is 2, but allowed 1.",
+				"71: Distance between variable 'count' declaration and its first usage is 4, but allowed 1.",
+				"96: Distance between variable 'arg' declaration and its first usage is 2, but allowed 1.",
+				"144: Distance between variable 'm' declaration and its first usage is 3, but allowed 1.",
+				"145: Distance between variable 'n' declaration and its first usage is 2, but allowed 1.",
+				"184: Distance between variable 'result' declaration and its first usage is 2, but allowed 1.",
+				"219: Distance between variable 't' declaration and its first usage is 5, but allowed 1.",
+				"222: Distance between variable 'c' declaration and its first usage is 3, but allowed 1.",
+				"223: Distance between variable 'd2' declaration and its first usage is 3, but allowed 1.",
+				"260: Distance between variable 'selected' declaration and its first usage is 2, but allowed 1.",
+				"261: Distance between variable 'model' declaration and its first usage is 2, but allowed 1.",
+				"287: Distance between variable 'sw' declaration and its first usage is 2, but allowed 1.",
+				"300: Distance between variable 'wh' declaration and its first usage is 2, but allowed 1.",
+				"343: Distance between variable 'green' declaration and its first usage is 2, but allowed 1.",
+                "344: Distance between variable 'blue' declaration and its first usage is 3, but allowed 1.",
 		};
 		verify(checkConfig, getPath("design" + File.separator + "InputVariableDeclarationUsageDistanceCheck.java"),
 				expected);
