d01b1ed8fff30f0309231e15a2cf014cd146c9c5
Daniil Yaroslavtsev <Daniil.Yaroslavtsev@gmail.com>
2012-02-27 21:15:22 +0200

    ForbidCertainImportsCheck was tested on huge sources and reay to code review.

--- eclipsecs-sevntu-plugin/src/com/github/sevntu/checkstyle/checks/coding/ForbidCertainImportsCheck.java
+++ eclipsecs-sevntu-plugin/src/com/github/sevntu/checkstyle/checks/coding/ForbidCertainImportsCheck.java
@@ -10,13 +10,26 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 /**
  * Forbids usage Certain Imports usage in class. <br>
  * Idea was taken from similar check in Sonar project. <br>
+ * <br>
  * Parameters:
  * <ol>
  * <li>Package name. (type=regexp)</li>
- * <li>Forbidden package name in imports (type=regexp) </li></ol>
- * Example of usage: Forbid
- * to use "*.ui.*" packages in "*.dao.*" packages By means of few instances of
- * this check will be possible to check any number of rules.<br><br>
+ * <li>Forbidden package name in imports (type=regexp)</li>
+ * </ol>
+ * Check retrieves package and imports text for any *.java file in string
+ * representation without "package"/"import" words or semicolons. So you can be
+ * sure to write regular expressions for package/imports text filtering. <br>
+ * <br>
+ * Example of usage: Forbid to use "*.ui.*" packages in "*.dao.*" packages: <br>
+ * <br>
+ * <dl>
+ * <li>Package name regexp = ".+.ui..+"</li>
+ * <li>Forbidden import regexp = ".+.dao..+"</li>
+ * </dl>
+ * <br>
+ * By means of few instances of this check will be possible to check any number
+ * of rules.<br>
+ * <br>
  * @author <a href="mailto:Daniil.Yaroslavtsev@gmail.com"> Daniil
  *         Yaroslavtsev</a>
  */
@@ -24,27 +37,30 @@ public class ForbidCertainImportsCheck extends Check
 {
 
     /**
-     * option
+     * Pattern object is used to store the regexp for the package text checking.
      */
     private Pattern mPackageNameRegexp;
 
     /**
-     * option
+     * Pattern object is used to store the regexp for the forbidden import text checking.
      */
     private Pattern mForbiddenImportRegexp;
 
     /**
-     * 
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
      */
     private String mKey = "forbid.certain.imports";
 
     /**
-     * 
-     */    
+     * Boolean flag. True, if the current package text is being processed
+     * matches the given regexp.
+     */
     private boolean packageMatches;
 
     /**
-     * @return the mPackageName
+     * Gets the regexp is currently used for the package text checking.
+     * @return The Pattern object is used to store the regexp for the package text checking.
      */
     public String getPackageNameRegexp()
     {
@@ -52,18 +68,20 @@ public class ForbidCertainImportsCheck extends Check
     }
 
     /**
+     * Sets the regexp is currently using for the package text checking.
      * @param aPackageNameRegexp
-     *        the mPackageName to set
+     *        String contains the regex to set for the package text checking.
      */
     public void setPackageNameRegexp(String aPackageNameRegexp)
     {
-        if (aPackageNameRegexp != null && !("".equals(aPackageNameRegexp))) {
+        if (aPackageNameRegexp != null) {
             mPackageNameRegexp = Pattern.compile(aPackageNameRegexp);
         }
     }
 
     /**
-     * @return the forbiddenPackageName
+     * Gets the regexp is currently used for the imports text checking.
+     * @return the regexp String is used for the imports text checking.
      */
     public String getForbiddenImportRegexp()
     {
@@ -71,13 +89,14 @@ public class ForbidCertainImportsCheck extends Check
     }
 
     /**
+     * Sets the regexp is currently used for the imports text checking.
      * @param aForbiddenPackageNameRegexp
-     *        the forbidden Package name to set
+     *        String contains a regex to set for the imports text checking.
      */
     public void setForbiddenImportRegexp(String
             aForbiddenPackageNameRegexp)
     {
-        if (aForbiddenPackageNameRegexp != null && !("".equals(aForbiddenPackageNameRegexp))) {
+        if (aForbiddenPackageNameRegexp != null) {
             mForbiddenImportRegexp = Pattern.compile(aForbiddenPackageNameRegexp);
         }
     }
@@ -112,28 +131,29 @@ public class ForbidCertainImportsCheck extends Check
     }
 
     /**
-     * Gets package/import text from the given DetailAST PaCKAGE_DEF or IMPORT
+     * Gets package/import text from the given DetailAST PACKAGE_DEF or IMPORT
      * node.
-     * @param packageOrImportAST
+     * @param aAST
+     *        - DetailAST node is pointing to package or import definition
+     *        (should be a PACKAGE_DEF or IMPORT type).
      */
-    private String getText(DetailAST packageOrImportAST)
+    private static String getText(DetailAST aAST)
     {
         String result = null;
-        DetailAST packageIdent = packageOrImportAST
-                .findFirstToken(TokenTypes.IDENT);
-        if (packageIdent == null) {
-            DetailAST packagePathAST = packageOrImportAST
-                    .findFirstToken(TokenTypes.DOT);
-            if (packagePathAST != null) {
-                FullIdent packagePathIdent = FullIdent
-                        .createFullIdentBelow(packagePathAST);
-                DetailAST packageName = packagePathAST.getLastChild();
-                result = packagePathIdent.getText() + "."
-                        + packageName.getText();
+
+        DetailAST ident = aAST.findFirstToken(TokenTypes.IDENT);
+
+        if (ident == null) {
+            final DetailAST firstDotAST = aAST.findFirstToken(TokenTypes.DOT);
+            if (firstDotAST != null) {
+                final FullIdent fullPathIdent = FullIdent
+                        .createFullIdentBelow(firstDotAST);
+                final DetailAST nameAST = firstDotAST.getLastChild();
+                result = fullPathIdent.getText() + "." + nameAST.getText();
             }
         }
         else {
-            result = packageIdent.getText();
+            result = ident.getText();
         }
         return result;
     }
