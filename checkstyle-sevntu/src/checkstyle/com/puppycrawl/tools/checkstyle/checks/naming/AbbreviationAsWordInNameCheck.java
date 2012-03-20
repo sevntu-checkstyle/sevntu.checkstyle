9e36a989273b51ce976c2f50df0722bac7982a39
Daniil Yaroslavtsev <Daniil.Yaroslavtsev@gmail.com>
2012-03-20 20:52:34 +0200

    AbbreviationAsWordInTypeNameCheck: option to disable warnings on methods with "Override" annotation was added. fixes Issue #23.

--- checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
+++ checkstyle-sevntu/src/checkstyle/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
@@ -33,25 +33,25 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * <p>
  * Check name of the targeted item to validate abbreviations(capital letters) length in it.
  * Options:
- * 		Allowed Abbreviation Length - allowed abbreviation length(length of capital characters).
- * 		Allowed Abbreviations - list of abbreviations separated by comma, without spaces.
+ *         Allowed Abbreviation Length - allowed abbreviation length(length of capital characters).
+ *         Allowed Abbreviations - list of abbreviations separated by comma, without spaces.
  * </p>
  * @author Roman Ivanov
  */
 public class AbbreviationAsWordInNameCheck extends Check {
 
-	/**
-	 * mAnnotationTargets is array of type forbidden annotation's target.
-	 */
-	private int[] mTargets = new int[0];
+    /**
+     * mAnnotationTargets is array of type forbidden annotation's target.
+     */
+    private int[] mTargets = new int[0];
 
-	/** Variable indicates on the allowed amount of capital letters in abbreviations in the classes, interfaces, variables and methods names. */
-	private int mAllowedAbbreviationLength = 3;
+    /** Variable indicates on the allowed amount of capital letters in abbreviations in the classes, interfaces, variables and methods names. */
+    private int mAllowedAbbreviationLength = 3;
 
-	/**
-	 * Set of allowed abbreviation to ignore in check
-	 */
-	private Set<String> mAllowedAbbreviations = new HashSet<String>();
+    /**
+     * Set of allowed abbreviation to ignore in check
+     */
+    private Set<String> mAllowedAbbreviations = new HashSet<String>();
 
     /** Allows to ignore variables with 'final' modifier. */
     private boolean mIgnoreFinal = true;
@@ -59,6 +59,9 @@ public class AbbreviationAsWordInNameCheck extends Check {
     /** Allows to ignore variables with 'static' modifier. */
     private boolean mIgnoreStatic = true;
 
+    /** Allows to ignore methods with '@Override' annotation. */
+    private boolean mIgnoreOverriddenMethods = true;
+    
     /**
      * Sets ignore option for variables with 'final' modifier.
      * @param aIgnoreFinal
@@ -71,84 +74,96 @@ public class AbbreviationAsWordInNameCheck extends Check {
 
     /**
      * Sets ignore option for variables with 'static' modifier.
-     * @param aIgnoreStatic
+     * @param aIgnoreFinal
      *        Defines if ignore variables with 'static' modifier or not.
      */
     public void setIgnoreStatic(boolean aIgnoreStatic)
     {
         this.mIgnoreStatic = aIgnoreStatic;
     }
+    
+    /**
+     * Sets ignore option for methods with "@Override" annotation.
+     * @param aIgnoreOverriddenMethods
+     *        Defines if ignore methods with "@Override" annotation or not.
+     */
+    public void setIgnoreOverriddenMethods(boolean aIgnoreOverriddenMethods)
+    {
+        this.mIgnoreOverriddenMethods = aIgnoreOverriddenMethods;
+    }
+    
+    /**
+     * Targets for a Check.
+     * 
+     * @param aTargets
+     *            - array of type's names
+     */
+    public void setTargets(String[] aTargets) {
+        if (aTargets != null) {
+            mTargets = new int[aTargets.length];
+            for (int i = 0; i < aTargets.length; i++) {
+                mTargets[i] = TokenTypes.getTokenId(aTargets[i]);
+            }
+        }
+    }
 
     /**
-	 * Targets for a Check.
-	 * 
-	 * @param aTargets
-	 *            - array of type's names
-	 */
-	public void setTargets(String[] aTargets) {
-		if (aTargets != null) {
-			mTargets = new int[aTargets.length];
-			for (int i = 0; i < aTargets.length; i++) {
-				mTargets[i] = TokenTypes.getTokenId(aTargets[i]);
-			}
-		}
-	}
+     * Allowed abbreviation length in names.
+     * 
+     * @param aAllowedAbbreviationLength
+     *            amount of allowed capital letters in abbreviation.
+     */
+    public void setAllowedAbbreviationLength(int aAllowedAbbreviationLength) {
+        mAllowedAbbreviationLength = aAllowedAbbreviationLength;
+    }
 
-	/**
-	 * Allowed abbreviation length in names.
-	 * 
-	 * @param aAllowedAbbreviationLength
-	 *            amount of allowed capital letters in abbreviation.
-	 */
-	public void setAllowedAbbreviationLength(int aAllowedAbbreviationLength) {
-		mAllowedAbbreviationLength = aAllowedAbbreviationLength;
-	}
+    /**
+     * Set a list of abbreviations that must be skipped for checking.
+     * Abbreviations should be separated by comma, no spaces is allowed.
+     * 
+     * @param aAllowedAbbr
+     *            an string of abbreviations that must be skipped from checking, each
+     *            abbreviation separated by comma.
+     */
+    public void setAllowedAbbreviations(String aAllowedAbbreviations) {
+        if (aAllowedAbbreviations != null) {
+            mAllowedAbbreviations = new HashSet<String>(Arrays.asList(aAllowedAbbreviations.split(",")));
+        }
+    }
 
-	/**
-	 * Set a list of abbreviations that must be skipped for checking.
-	 * Abbreviations should be separated by comma, no spaces is allowed.
-	 * 
-	 * @param aAllowedAbbr
-	 *            an string of abbreviations that must be skipped from checking, each
-	 *            abbreviation separated by comma.
-	 */
-	public void setAllowedAbbreviations(String aAllowedAbbreviations) {
-		if (aAllowedAbbreviations != null) {
-			mAllowedAbbreviations = new HashSet<String>(Arrays.asList(aAllowedAbbreviations.split(",")));
-		}
-	}
+    @Override
+    public int[] getDefaultTokens() {
+        return mTargets;
+    }
 
-	@Override
-	public int[] getDefaultTokens() {
-		return mTargets;
-	}
+    @Override
+    public void visitToken(DetailAST aAst) {
 
-	@Override
-	public void visitToken(DetailAST aAst) {
+        if (!isIgnoreSituation(aAst)) {
+            
+            final DetailAST nameAst = aAst.findFirstToken(TokenTypes.IDENT);
 
-		if (!isIgnoreSituation(aAst)) {
-			
-			final DetailAST nameAst = aAst.findFirstToken(TokenTypes.IDENT);
+            final String typeName = nameAst.getText();
 
-			final String typeName = nameAst.getText();
+            String abbr = getDisallowedAbbreviation(typeName);
+            if (abbr != null) {
+                log(nameAst.getLineNo(),
+                        "abbreviationAsWord",
+                        mAllowedAbbreviationLength);
+            }
+        }        
+    }
 
-			String abbr = getDisallowedAbbreviation(typeName);
-			if (abbr != null) {
-				log(nameAst.getLineNo(),
-						"abbreviationAsWord",
-						mAllowedAbbreviationLength);
-			}
-        }		
-	}
+    private boolean isIgnoreSituation(DetailAST aAst)
+    {
+        final DetailAST modifiers = aAst.getFirstChild();
 
-	private boolean isIgnoreSituation(DetailAST aAst) {
-		final DetailAST modifiers = aAst.getFirstChild();
-
-		return aAst.getType() == TokenTypes.VARIABLE_DEF
-				&& ((mIgnoreFinal && modifiers.branchContains(TokenTypes.FINAL))
-						|| (mIgnoreStatic && modifiers.branchContains(TokenTypes.LITERAL_STATIC)))
-				|| (aAst.getType() == TokenTypes.METHOD_DEF && hasOverrideAnnotation(modifiers));
-	}
+        return aAst.getType() == TokenTypes.VARIABLE_DEF
+                && ((mIgnoreFinal && modifiers.branchContains(TokenTypes.FINAL))
+                || (mIgnoreStatic && modifiers.branchContains(TokenTypes.LITERAL_STATIC)))
+                || (mIgnoreOverriddenMethods && aAst.getType()
+                            == TokenTypes.METHOD_DEF && hasOverrideAnnotation(modifiers));
+    }
 
     /**
      * Checks that the method has "@Override" annotation.
@@ -162,7 +177,8 @@ public class AbbreviationAsWordInNameCheck extends Check {
         boolean result = false;
         for (DetailAST child : getChildren(aMethodModifiersAST)) {
             if (child.getType() == TokenTypes.ANNOTATION) {
-                String annotationText = child.findFirstToken(TokenTypes.IDENT).getText();
+                final String annotationText =
+                        child.findFirstToken(TokenTypes.IDENT).getText();
                 if ("Override".equals(annotationText)) {
                     result = true;
                     break;
@@ -173,51 +189,51 @@ public class AbbreviationAsWordInNameCheck extends Check {
     }
 
     private String getDisallowedAbbreviation(String string) {
-		int beginIndex = 0;
-		boolean abbrStarted = false;
-		String result = null;
-		
-		for(int index = 0 ; index < string.length(); index ++) {
-			char symbol = string.charAt(index);
-			
-			if (Character.isUpperCase(symbol)) {
-				if (!abbrStarted) {
-					abbrStarted = true;
-					beginIndex = index;
-				}
-			} else {
-				if (abbrStarted) {
-					abbrStarted = false;
-					
-					// -1 as a first capital is usually beginning of next word 
-					int endIndex = index - 1;
-					int abbrLength = endIndex - beginIndex;
-					if (abbrLength > mAllowedAbbreviationLength) {
-						result = string.substring(beginIndex, endIndex);
-						if (!mAllowedAbbreviations.contains(result)) {
-							break;
-						} else {
-							result = null;
-						}
-					}
-					
-					beginIndex = -1;
-				}
-			}
-		}
-		if (abbrStarted) {
-			int endIndex = string.length();
-			int abbrLength = endIndex - beginIndex;
-			if (abbrLength > mAllowedAbbreviationLength) {
-				result = string.substring(beginIndex, endIndex);
-				if (mAllowedAbbreviations.contains(result)) {
-					result = null;
-				}
-			}
-		}
-		return result;
-	}
-	
+        int beginIndex = 0;
+        boolean abbrStarted = false;
+        String result = null;
+        
+        for(int index = 0 ; index < string.length(); index ++) {
+            char symbol = string.charAt(index);
+            
+            if (Character.isUpperCase(symbol)) {
+                if (!abbrStarted) {
+                    abbrStarted = true;
+                    beginIndex = index;
+                }
+            } else {
+                if (abbrStarted) {
+                    abbrStarted = false;
+                    
+                    // -1 as a first capital is usually beginning of next word 
+                    int endIndex = index - 1;
+                    int abbrLength = endIndex - beginIndex;
+                    if (abbrLength > mAllowedAbbreviationLength) {
+                        result = string.substring(beginIndex, endIndex);
+                        if (!mAllowedAbbreviations.contains(result)) {
+                            break;
+                        } else {
+                            result = null;
+                        }
+                    }
+                    
+                    beginIndex = -1;
+                }
+            }
+        }
+        if (abbrStarted) {
+            int endIndex = string.length();
+            int abbrLength = endIndex - beginIndex;
+            if (abbrLength > mAllowedAbbreviationLength) {
+                result = string.substring(beginIndex, endIndex);
+                if (mAllowedAbbreviations.contains(result)) {
+                    result = null;
+                }
+            }
+        }
+        return result;
+    }
+    
     /**
      * Gets all the children which are one level below on the current DetailAST
      * parent node.
@@ -235,5 +251,5 @@ public class AbbreviationAsWordInNameCheck extends Check {
         }
         return result;
     }
-    
+
 }
--- checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheckTest.java
+++ checkstyle-sevntu/src/tests/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheckTest.java
@@ -11,125 +11,134 @@ public class AbbreviationAsWordInNameCheckTest extends BaseCheckTestSupport {
 
 	private final String message = "%d: Abbreviation in name must contain no more than '%d' capital letters.";
 	
-//	@Test
-//	public void testTypeNamesForThreePermitedCapitalLetters() throws Exception {
-//
-//		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
-//		final int expectedCapitalCount = 3;
-//		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-//		checkConfig.addAttribute("allowedAbbreviations", "III");
-//		checkConfig.addAttribute("targets", "CLASS_DEF"
-//				//",VARIABLE_DEF" +
-//				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
-//				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
-//				);
-//
-//		final String[] expected = {
-//				// message <Linenumber> <expected capital count>
-//				String.format(message, 9, expectedCapitalCount),
-//				String.format(message, 12, expectedCapitalCount),
-//				String.format(message, 32, expectedCapitalCount),
-//				String.format(message, 37, expectedCapitalCount) };
-//
-//		verify(checkConfig, getPath("naming" + File.separator
-//				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
-//	}
-//
-//	@Test
-//	public void testTypeNamesForFourPermitedCapitalLetters() throws Exception {
-//
-//		final int expectedCapitalCount = 4;
-//		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
-//		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-//		checkConfig.addAttribute("allowedAbbreviations", "CLASS,FACTORY");
-//		checkConfig.addAttribute("targets", "CLASS_DEF"
-//				//",VARIABLE_DEF" +
-//				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
-//				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
-//				);
-//		final String[] expected = {
-//				// message <Linenumber> <expected capital count>
-//				String.format(message, 32, expectedCapitalCount) };
-//
-//		verify(checkConfig, getPath("naming" + File.separator
-//				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
-//	}
-//
-//	@Test
-//	public void testTypeNamesForFivePermitedCapitalLetters() throws Exception {
-//
-//		final int expectedCapitalCount = 5;
-//		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
-//		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-//		checkConfig.addAttribute("allowedAbbreviations", "CLASS");
-//		checkConfig.addAttribute("targets", "CLASS_DEF"
-//				//",VARIABLE_DEF" +
-//				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
-//				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
-//				);
-//		final String[] expected = {
-//				// message <Linenumber> <expected capital count>
-//				String.format(message, 32, expectedCapitalCount),
-//				String.format(message, 37, expectedCapitalCount),
-//			};
-//
-//		verify(checkConfig, getPath("naming" + File.separator
-//				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
-//	}
-//
-//	@Test
-//	public void testTypeAndVariablesAndMethodNames() throws Exception {
-//
-//		final int expectedCapitalCount = 5;
-//		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
-//		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-//		checkConfig.addAttribute("allowedAbbreviations", "CLASS");
-//		checkConfig.addAttribute("targets", "CLASS_DEF" + 
-//				",VARIABLE_DEF" +
-//				",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
-//				",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
-//				);
-//		final String[] expected = {
-//				// message <Linenumber> <expected capital count>
-//				String.format(message, 32, expectedCapitalCount),
-//				String.format(message, 37, expectedCapitalCount),
-//				String.format(message, 38, expectedCapitalCount),
-//				String.format(message, 39, expectedCapitalCount),
-//				String.format(message, 40, expectedCapitalCount),
-//				String.format(message, 58, expectedCapitalCount),
-//			};
-//
-//		verify(checkConfig, getPath("naming" + File.separator
-//				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
-//	}
-//	
-//	@Test
-//	public void testTypeAndVariablesAndMethodNamesWithIgnores() throws Exception {
-//
-//		final int expectedCapitalCount = 5;
-//		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
-//		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-//		checkConfig.addAttribute("allowedAbbreviations", "NUMBER,MARAZMATIC,VARIABLE");
-//		checkConfig.addAttribute("ignoreStatic", "false");
-//		checkConfig.addAttribute("ignoreFinal", "false");
-//		checkConfig.addAttribute("targets", "CLASS_DEF" + 
-//				",VARIABLE_DEF" +
-//				",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
-//				",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
-//				);
-//		final String[] expected = {
-//				// message <Linenumber> <expected capital count>
-//				String.format(message, 32, expectedCapitalCount),
-//				String.format(message, 37, expectedCapitalCount),
-//				String.format(message, 38, expectedCapitalCount),
-////				String.format(message, 59, expectedCapitalCount),
-////				String.format(message, 60, expectedCapitalCount),
-////				String.format(message, 61, expectedCapitalCount),
-//			};
-//
-//		verify(checkConfig, getPath("naming" + File.separator
-//				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
-//	}
+	@Test
+	public void testTypeNamesForThreePermitedCapitalLetters() throws Exception {
+
+		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
+		final int expectedCapitalCount = 3;
+		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
+		checkConfig.addAttribute("allowedAbbreviations", "III");
+		checkConfig.addAttribute("targets", "CLASS_DEF"
+				//",VARIABLE_DEF" +
+				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
+				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
+				);
+		checkConfig.addAttribute("ignoreOverriddenMethods", "true");
+
+		final String[] expected = {
+				// message <Linenumber> <expected capital count>
+				String.format(message, 9, expectedCapitalCount),
+				String.format(message, 12, expectedCapitalCount),
+				String.format(message, 32, expectedCapitalCount),
+				String.format(message, 37, expectedCapitalCount) };
+
+		verify(checkConfig, getPath("naming" + File.separator
+				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
+	}
+
+	@Test
+	public void testTypeNamesForFourPermitedCapitalLetters() throws Exception {
+
+		final int expectedCapitalCount = 4;
+		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
+		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
+		checkConfig.addAttribute("allowedAbbreviations", "CLASS,FACTORY");
+		checkConfig.addAttribute("targets", "CLASS_DEF"
+				//",VARIABLE_DEF" +
+				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
+				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
+				);
+		checkConfig.addAttribute("ignoreOverriddenMethods", "true");
+		
+		final String[] expected = {
+				// message <Linenumber> <expected capital count>
+				String.format(message, 32, expectedCapitalCount) };
+
+		verify(checkConfig, getPath("naming" + File.separator
+				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
+	}
+
+	@Test
+	public void testTypeNamesForFivePermitedCapitalLetters() throws Exception {
+
+		final int expectedCapitalCount = 5;
+		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
+		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
+		checkConfig.addAttribute("allowedAbbreviations", "CLASS");
+		checkConfig.addAttribute("targets", "CLASS_DEF"
+				//",VARIABLE_DEF" +
+				//",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
+				//",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
+				);
+		checkConfig.addAttribute("ignoreOverriddenMethods", "true");
+		
+		final String[] expected = {
+				// message <Linenumber> <expected capital count>
+				String.format(message, 32, expectedCapitalCount),
+				String.format(message, 37, expectedCapitalCount),
+			};
+
+		verify(checkConfig, getPath("naming" + File.separator
+				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
+	}
+
+	@Test
+	public void testTypeAndVariablesAndMethodNames() throws Exception {
+
+		final int expectedCapitalCount = 5;
+		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
+		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
+		checkConfig.addAttribute("allowedAbbreviations", "CLASS");
+		checkConfig.addAttribute("targets", "CLASS_DEF" + 
+				",VARIABLE_DEF" +
+				",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
+				",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
+				);
+		checkConfig.addAttribute("ignoreOverriddenMethods", "true");
+		
+		final String[] expected = {
+				// message <Linenumber> <expected capital count>
+				String.format(message, 32, expectedCapitalCount),
+				String.format(message, 37, expectedCapitalCount),
+				String.format(message, 38, expectedCapitalCount),
+				String.format(message, 39, expectedCapitalCount),
+				String.format(message, 40, expectedCapitalCount),
+				String.format(message, 58, expectedCapitalCount),
+			};
+
+		verify(checkConfig, getPath("naming" + File.separator
+				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
+	}
+	
+	@Test
+	public void testTypeAndVariablesAndMethodNamesWithIgnores() throws Exception {
+
+		final int expectedCapitalCount = 5;
+		final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
+		checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
+		checkConfig.addAttribute("allowedAbbreviations", "NUMBER,MARAZMATIC,VARIABLE");
+		checkConfig.addAttribute("ignoreStatic", "false");
+		checkConfig.addAttribute("ignoreFinal", "false");
+		checkConfig.addAttribute("targets", "CLASS_DEF" + 
+				",VARIABLE_DEF" +
+				",METHOD_DEF,ENUM_DEF,ENUM_CONSTANT_DEF" +
+				",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
+				);
+		checkConfig.addAttribute("ignoreOverriddenMethods", "true");
+		
+		final String[] expected = {
+				// message <Linenumber> <expected capital count>
+				String.format(message, 32, expectedCapitalCount),
+				String.format(message, 37, expectedCapitalCount),
+				String.format(message, 38, expectedCapitalCount),
+//				String.format(message, 59, expectedCapitalCount),
+//				String.format(message, 60, expectedCapitalCount),
+//				String.format(message, 61, expectedCapitalCount),
+			};
+
+		verify(checkConfig, getPath("naming" + File.separator
+				+ "InputAbbreviationAsWordInTypeNameCheck.java"), expected);
+	}
 	
 	   @Test
 	    public void testTypeNamesForThreePermitedCapitalLettersWithOverridenMethod() throws Exception {
@@ -137,12 +146,13 @@ public class AbbreviationAsWordInNameCheckTest extends BaseCheckTestSupport {
 	        final DefaultConfiguration checkConfig = createCheckConfig(AbbreviationAsWordInNameCheck.class);
 	        final int expectedCapitalCount = 3;
 	        checkConfig.addAttribute("allowedAbbreviationLength", String.valueOf(expectedCapitalCount));
-	        checkConfig.addAttribute("allowedAbbreviations", "III");
+	        checkConfig.addAttribute("allowedAbbreviations", "");
 	        checkConfig.addAttribute("targets", "CLASS_DEF, METHOD_DEF"
 	                //",VARIABLE_DEF" +
 	                //",,ENUM_DEF,ENUM_CONSTANT_DEF" +
 	                //",PARAMETER_DEF,INTERFACE_DEF,ANNOTATION_DEF"
 	                );
+	        checkConfig.addAttribute("ignoreOverriddenMethods", "true");
 
         final String[] expected = {
                 };
