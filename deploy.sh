2574e0eb7e49f2410f78e328c74cff7b374f20b4
sabaka <sabaka1391@gmail.com>
2012-07-13 16:14:50 +0300

    Deploy shell-script was created

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,9 @@ mvn deploy
 cd ../sevntu-checkstyle-maven-plugin/
 mvn deploy
 cd ..
+git stash
 git checkout gh-pages
+git rm maven2/*
 cp -r ../maven2 ./
+git add maven2/*
+echo 'You are on the gh-pages branch! Your changes was stashed, type git stash apply, when you return on your branch."
