20611d32cef4c931602767e786ad80917cf88839
Ivan Sopov <moradan228@gmail.com>
2012-08-09 21:13:18 +0300

    merge autodeployment of eclipse-update site

---
+++ deploy-eclipse.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#clean
+rm -rf gh-pages
+
+#prepare
+mkdir gh-pages
+cd ./gh-pages
+
+git init
+git remote add origin https://github.com/sevntu-checkstyle/sevntu.checkstyle.git
+git fetch origin gh-pages:refs/remotes/origin/gh-pages
+git checkout gh-pages
+
+cd ..
+echo -n "Enter version number"
+read version
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$version
+mvn clean install
+cd update-site
+mvn wagon:upload
+
+#git add .
+#git commit -m"new version deploy"
+#git push
