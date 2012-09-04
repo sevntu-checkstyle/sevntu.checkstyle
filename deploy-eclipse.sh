8abe5883cc7285b787fbeda41ba84dc29e0cbae3
Ivan Sopov <isopov@reveredata.com>
2012-09-04 11:34:46 +0300

    delete all remnants of AbstractClassNameCheck from our source since all the changes were merged to Checkstyle upstreadm, prepare 1.5.2 release

--- deploy-eclipse.sh
+++ deploy-eclipse.sh
@@ -13,10 +13,10 @@ git fetch origin gh-pages:refs/remotes/origin/gh-pages
 git checkout gh-pages
 
 cd ..
-echo -n "Enter version number"
-read version
-mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$version
-mvn clean install
+#echo -n "Enter version number"
+#read version
+#mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$version -f eclipse-pom.xml
+mvn clean install -f eclipse-pom.xml
 cd update-site
 mvn wagon:upload
 
