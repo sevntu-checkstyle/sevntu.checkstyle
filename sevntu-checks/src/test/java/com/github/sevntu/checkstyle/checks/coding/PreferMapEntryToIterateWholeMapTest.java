f97b5f2afe68a75f74ab72fc757e5e9fffc8b035
maxvetrenko <maxvetrenko2241@gmail.com>
2013-10-03 15:36:37 +0300

    Fixed #77. PreferMapEntryToIterateWholeMapCheck was introduced

---
+++ sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/PreferMapEntryToIterateWholeMapTest.java
@@ -0,0 +1,104 @@
+package com.github.sevntu.checkstyle.checks.coding;
+
+import org.junit.Test;
+
+import com.github.sevntu.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+public class PreferMapEntryToIterateWholeMapTest extends BaseCheckTestSupport
+{
+
+    /**
+     * Default check configuration
+     */
+    private final DefaultConfiguration mCheckConfig =
+        createCheckConfig(PreferMapEntryToIterateWholeMapCheck.class);
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file
+     */
+    private final String mMsgKeyEntrySet =
+            PreferMapEntryToIterateWholeMapCheck.MSG_KEY_ENTRYSET;
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file
+     */
+    private final String mMsgKeyValues =
+            PreferMapEntryToIterateWholeMapCheck.MSG_KEY_VALUES;
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file
+     */
+    private final String mMsgKeyKeySet =
+            PreferMapEntryToIterateWholeMapCheck.MSG_KEY_KEYSET;
+
+    @Test
+    public final void basicTest() throws Exception
+    {
+        mCheckConfig.addAttribute("processingKeySet", "true");
+        mCheckConfig.addAttribute("processingEntrySet", "true");
+
+        final String[] expected = {
+            "22:25: "
+                    + getCheckMessage(mMsgKeyEntrySet),
+            "45:25: "
+                    + getCheckMessage(mMsgKeyValues),
+            "47:33: "
+                    + getCheckMessage(mMsgKeyKeySet),
+            "71:33: "
+                    + getCheckMessage(mMsgKeyEntrySet),
+            "76:25: "
+                    + getCheckMessage(mMsgKeyKeySet),
+            "83:17: "
+                    + getCheckMessage(mMsgKeyEntrySet),
+            "88:17: "
+                    + getCheckMessage(mMsgKeyValues), };
+
+        verify(mCheckConfig,
+                getPath("InputPreferMapEntryToIterateWholeMap.java"), expected);
+    }
+
+    @Test
+    public final void importsWithoutFullPathTest() throws Exception
+    {
+        mCheckConfig.addAttribute("processingKeySet", "true");
+        mCheckConfig.addAttribute("processingEntrySet", "true");
+
+        final String supportedMapImplementationQualifiedNames =
+                "java.util.Map, "
+                + "java.util.HashMap, java.util.TreeMap, com.myTest.MyMap, ";
+
+        mCheckConfig.addAttribute("supportedMapImplementationQualifiedNames",
+                supportedMapImplementationQualifiedNames);
+        final String [] expected = {
+            "11:9: " + getCheckMessage(mMsgKeyEntrySet),
+            "16:9: " + getCheckMessage(mMsgKeyValues), };
+        verify(mCheckConfig,
+                getPath("InputPreferMapEntryToIterateWholeMapImport.java"),
+                expected);
+    }
+
+
+    @Test
+    public final void skipIfConditionTest() throws Exception
+    {
+        mCheckConfig.addAttribute("processingKeySet", "true");
+        mCheckConfig.addAttribute("processingEntrySet", "true");
+
+        final String supportedMapImplementationQualifiedNames =
+                "java.util.Map, "
+                + "java.util.HashMap, java.util.TreeMap, "
+                + "com.myTest.MyMap";
+
+        mCheckConfig.addAttribute("supportedMapImplementationQualifiedNames",
+                supportedMapImplementationQualifiedNames);
+
+        final String [] expected =
+        {
+                "12:9: " + getCheckMessage(mMsgKeyEntrySet), 
+                "28:9: " + getCheckMessage(mMsgKeyValues), };
+        verify(mCheckConfig,
+                getPath("InputPreferMapEntryToIterateWholeMapSkipIf.java"),
+                expected);
+    }
+}
