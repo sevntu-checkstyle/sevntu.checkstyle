00cb69f6da546e462ee376f86d6f86d29ba652b1
alexkravin <nesterenko-aleksey@list.ru>
2014-08-22 00:04:44 +0400

    LogicConditionsNeedOptimizationCheck_195

--- sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/coding/InputLogicConditionsNeedOptimizationCheck.java
+++ sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/coding/InputLogicConditionsNeedOptimizationCheck.java
@@ -1,4 +1,4 @@
-
+package com.github.sevntu.checkstyle.checks.coding;
 public class InputLogicConditionsNeedOptimizationCheck
 {
 	private boolean field1;
@@ -58,34 +58,19 @@ public class InputLogicConditionsNeedOptimizationCheck
 		if(returnTrue() && returnFalse()){}
 		if(returnTrue() && returnFalse() && field1){} //!!
 		if(returnFalse() && field1 && 1 + 2 - 3 == 0 || field1 && returnTrue()){} //!!
-		if (comp.isFocusable() && (!(comp instanceof Container) || ((Container) comp).getLayout() == null)) {
-            return comp;
-        }
-		if (Boolean.TRUE.equals(getClientProperty("JTable.autoStartsEdit"))
-                && (ks.getModifiers() & KeyEvent.CTRL_MASK) != 0
-                && (ks.getKeyCode() == KeyEvent.VK_S || ks.getKeyCode() == KeyEvent.VK_W)) {
-            return false;
-        }
-		if (!LdHelper.isSystemName(table.getName())
-                && (cliTable == null
-                || !PermHelper.nullOrEqual(cliTable.getPermission(), table.getPermission()))) {
+		 boolean field = false;
+		 if ("".equals("") && ("" == null || false)) {
 
-            forceReloadMetadata = true;
-        }
-		 if (refCol != null && refCol.getTable() != null
-                 && (tableName == null || tableName.equals(refCol.getTable().getName()))) {}
-		 if (!LdHelper.isSystemName(table.getName()) && (cliTable == null || false)) {
-
-	            forceReloadMetadata = true;
+	            field = true;
 	        }
 		 if(returnTrue() && field1
-		         || (field2 && returnFalse() && field1)){}
+		         || (field && returnFalse() && field1)){}
 		 if(returnTrue() && 
-		         (field2 && returnFalse() && field1)){}
+		         (field && returnFalse() && field1)){}
 		 if(returnTrue() && 
-                 (field2 && field1 && returnFalse())){}
+                 (field && field1 && returnFalse())){}
 		 if(returnTrue() && 
-                 (field2 && field1)){}
+                 (field && field1)){}
 	}
 	
 	public class Testing
