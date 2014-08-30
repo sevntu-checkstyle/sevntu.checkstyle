17f4fdd97a6b346db7e0d20cb0a6486b55efaddb
alexkravin <nesterenko-aleksey@list.ru>
2014-08-30 16:24:50 +0400

    Redundant Return Check Test updated formatting to match 'Checkstyle for Checkstyle config'

--- sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/RedundantReturnCheckTest.java
+++ sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/RedundantReturnCheckTest.java
@@ -12,13 +12,19 @@ public class RedundantReturnCheckTest extends BaseCheckTestSupport {
 	final DefaultConfiguration checkConfig = createCheckConfig(RedundantReturnCheck.class);
 	checkConfig.addAttribute("allowReturnInEmptyMethodsAndConstructors", "false");
 
-	final String[] expected = { "10: Redundant return.",
-		"17: Redundant return.", "22: Redundant return.",
-		"32: Redundant return.", "39: Redundant return.",
-		"52: Redundant return.", "56: Redundant return.",
-		"60: Redundant return.", "87: Redundant return.",
-		"100: Redundant return.", "104: Redundant return."
-	};
+	final String[] expected = {
+	        "10: Redundant return.",
+	        "17: Redundant return.",
+	        "22: Redundant return.",
+	        "32: Redundant return.",
+	        "39: Redundant return.",
+	        "52: Redundant return.",
+	        "56: Redundant return.",
+	        "60: Redundant return.",
+	        "87: Redundant return.",
+	        "100: Redundant return.",
+	        "104: Redundant return."
+	    };
 
 	verify(checkConfig, getPath("InputRedundantReturn.java"), expected);
     }
@@ -28,12 +34,16 @@ public class RedundantReturnCheckTest extends BaseCheckTestSupport {
 	final DefaultConfiguration checkConfig = createCheckConfig(RedundantReturnCheck.class);
 	checkConfig.addAttribute("allowReturnInEmptyMethodsAndConstructors", "true");
 
-	final String[] expected = { "17: Redundant return.",
-		"32: Redundant return.", "39: Redundant return.",
-		"52: Redundant return.", "56: Redundant return.",
-		"60: Redundant return.", "100: Redundant return.",
-		"104: Redundant return."
-	};
+	final String[] expected = { 
+	        "17: Redundant return.",
+	        "32: Redundant return.", 
+	        "39: Redundant return.",
+	        "52: Redundant return.", 
+	        "56: Redundant return.",
+	        "60: Redundant return.", 
+	        "100: Redundant return.",
+	        "104: Redundant return."
+	    };
 
 	verify(checkConfig, getPath("InputRedundantReturn.java"), expected);
 
