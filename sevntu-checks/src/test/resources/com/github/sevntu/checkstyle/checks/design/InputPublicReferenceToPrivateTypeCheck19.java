b57781b4bc559c0b3af5a8e15d80c124a9a61ace
alexkravin <nesterenko-aleksey@list.ru>
2014-09-03 16:44:26 +0400

    Public Reference To Private Type Check #195 Compilable UT inputs

--- sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/design/InputPublicReferenceToPrivateTypeCheck19.java
+++ sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/design/InputPublicReferenceToPrivateTypeCheck19.java
@@ -1,11 +1,11 @@
 package com.github.sevntu.checkstyle.checks.design;
-
+import java.util.Map;
 public class InputPublicReferenceToPrivateTypeCheck19 {
     public void render(int c, C1 d) {} // WARNING
     
-    public int render(C1 c) {}  //WARNING
+    public int render(C1 c) { return 0; }  //WARNING
     
-    protected void someMethod(List<String, C1> s) {}   //WARNING
+    protected void someMethod(Map<String, C1> s) {}   //WARNING
     
     private class C1{}
 }
