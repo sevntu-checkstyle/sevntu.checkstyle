f8355c594a7545872c6c4a2ba8d3645bb21d3c24
alexkravin <nesterenko-aleksey@list.ru>
2014-09-03 17:32:18 +0400

    CauseParameterInExceptionCheck#195 Compilable UT inputs

---
+++ sevntu-checks/src/test/resources/CauseParameterInExceptionCheck/utils/ParentException.java
@@ -0,0 +1,14 @@
+package CauseParameterInExceptionCheck.utils;
+
+public class ParentException
+{
+
+	public ParentException(Exception cause)
+	{
+	}
+
+	public ParentException(String str, Exception e)
+	{
+	}
+
+}
