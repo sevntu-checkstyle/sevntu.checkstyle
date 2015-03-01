3f13341b98e1b36d9cb65111dbb1bd7bbb2206c7
Damian Szczepanik <damian.publicemail@gmail.com>
2015-03-01 12:45:40 +0100

    Checking if there is no switch block inside other switch block.
    Issue #249

---
+++ sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/design/NestedSwitchCheck.java
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2015  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.github.sevntu.checkstyle.checks.design;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.checks.coding.AbstractNestedDepthCheck;
+
+/**
+ * <p>
+ * This check ensures that there is no switch block inside other switch block. In such case nested
+ * block should be exposed into new method.
+ * </p>
+ * Assuming following block:
+ * 
+ * <pre>
+ * <code>
+ *      switch (color) {
+ *      case GREEN:
+ *          switch (type) {
+ *          case MEDIUM:
+ *          }
+ *      case BLUE:
+ *      }
+ * </code>
+ * </pre>
+ * 
+ * Nested switch block that checks <code>type</code> parameter should be converted into separate
+ * method.<br>
+ * To enable this check use following configuration:<br>
+ * <br>
+ * <code>&lt;module name=&quot;NestedSwitchCheck&quot;/&gt;<code>
+ * <br><br>
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+public class NestedSwitchCheck extends AbstractNestedDepthCheck {
+
+    public static final String MSG_KEY = "avoid.nested.switch";
+
+    /** Default allowed nesting depth. */
+    private static final int DEFAULT_MAX = 0;
+
+    public NestedSwitchCheck() {
+        super(DEFAULT_MAX);
+    }
+
+    @Override
+    public int[] getDefaultTokens() {
+        return new int[] { TokenTypes.LITERAL_SWITCH };
+    }
+
+    @Override
+    public void visitToken(DetailAST aAST)
+    {
+        nestIn(aAST, MSG_KEY);
+    }
+
+    @Override
+    public void leaveToken(DetailAST aAST)
+    {
+        nestOut();
+    }
+}
---
+++ sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/design/NestedSwitchCheckTest.java
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2015  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.github.sevntu.checkstyle.checks.design;
+
+
+import static com.github.sevntu.checkstyle.checks.design.NestedSwitchCheck.MSG_KEY;
+
+import org.junit.Test;
+
+import com.github.sevntu.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+/**
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+public class NestedSwitchCheckTest extends BaseCheckTestSupport
+ {
+	private final DefaultConfiguration checkConfig = createCheckConfig(NestedSwitchCheck.class);
+
+	@Test
+	public void testSimple() throws Exception
+{
+		final String[] expected = {
+                "11:13: " + getCheckMessage(MSG_KEY),
+                "23:13: " + getCheckMessage(MSG_KEY),
+                "28:13: " + getCheckMessage(MSG_KEY),
+                "35:13: " + getCheckMessage(MSG_KEY),
+                "37:17: " + getCheckMessage(MSG_KEY),
+                "50:25: " + getCheckMessage(MSG_KEY),
+                "52:29: " + getCheckMessage(MSG_KEY),
+                "63:25: " + getCheckMessage(MSG_KEY),
+                "65:29: " + getCheckMessage(MSG_KEY),
+                };
+
+        verify(checkConfig, getPath("InputNestedSwitchCheck.java"),
+                expected);
+    }
+}
