0df3c3fd4a3b69fd5572ca4e2b02bcf95148ca6b
Roman Ivanov <ivanov-jr@mail.ru>
2016-06-04 18:01:23 -0700

    minor: version bump script turning

--- pom-version-bump.sh
+++ pom-version-bump.sh
@@ -11,7 +11,7 @@ update-site/pom.xml)
 for i in "${POM_FILES[@]}"
 do
 	echo $i
-	xmlstarlet ed -N pom="http://maven.apache.org/POM/4.0.0" -u "/pom:project/pom:version" -v $NEW_VERSION $i > $i.new
+	xmlstarlet ed --ps --pf -N pom="http://maven.apache.org/POM/4.0.0" -u "/pom:project/pom:version" -v $NEW_VERSION $i > $i.new
 	mv $i.new $i
 done
 
