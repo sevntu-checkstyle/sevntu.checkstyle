e23f8cee628ca13312e27b71891481b3ef6d4af6
Roman Ivanov <ivanov-jr@mail.ru>
2016-06-07 11:31:33 -0700

    minor: update for pom-version-bump to do all version bump

--- pom-version-bump.sh
+++ pom-version-bump.sh
@@ -5,32 +5,60 @@
 
 OLD_VERSION=$1
 NEW_VERSION=$2
+
+if [[ -z "$OLD_VERSION" || -z "$NEW_VERSION" ]]
+then
+    echo "Old and New version need to be specified"
+    exit 1
+fi
+
 POM_FILES=(eclipse-pom.xml
-eclipsecs-sevntu-plugin-feature/pom.xml
-eclipsecs-sevntu-plugin/pom.xml
 sevntu-checks/pom.xml
 sevntu-checkstyle-idea-extension/pom.xml
 sevntu-checkstyle-maven-plugin/pom.xml
 sevntu-checkstyle-sonar-plugin/pom.xml
-update-site/pom.xml)
+)
 for i in "${POM_FILES[@]}"
 do
 	echo "Updating: "$i
 	xmlstarlet ed --ps -N pom="http://maven.apache.org/POM/4.0.0" \
-	    -u "/pom:project/pom:version" -v $NEW_VERSION $i > $i.new
+	    -u "/pom:project/pom:version" -v $NEW_VERSION \
+	    $i > $i.new
+	mv $i.new $i
+done
+
+POM_FILES=(eclipsecs-sevntu-plugin-feature/pom.xml
+eclipsecs-sevntu-plugin/pom.xml
+update-site/pom.xml
+)
+for i in "${POM_FILES[@]}"
+do
+	echo "Updating: "$i
+	xmlstarlet ed --ps -N pom="http://maven.apache.org/POM/4.0.0" \
+	    -u "/pom:project/pom:parent/pom:version" -v $NEW_VERSION \
+	    $i > $i.new
 	mv $i.new $i
 done
 
 #aditional version reference
-POM_FILE=sevntu-checkstyle-maven-plugin/pom.xml
-echo "Updating: "$POM_FILE
+FILE=sevntu-checkstyle-maven-plugin/pom.xml
+echo "Updating: "$FILE
 xmlstarlet ed --ps -N pom="http://maven.apache.org/POM/4.0.0" \
-    -u '//pom:project/pom:dependencies/pom:dependency[pom:artifactId="sevntu-checks"]/pom:version' \
-    -v $NEW_VERSION $POM_FILE > $POM_FILE.new
-mv $POM_FILE.new $POM_FILE
+    -u '//pom:project/pom:dependencies/pom:dependency[pom:artifactId="sevntu-checks"]/pom:version' -v $NEW_VERSION \
+    $FILE > $FILE.new
+mv $FILE.new $FILE
 
 #special
-echo "Updating: MANIFEST.MF"
-sed -i "s/Bundle-Version: $OLD_VERSION/Bundle-Version: $NEW_VERSION/" eclipsecs-sevntu-plugin/META-INF/MANIFEST.MF
-echo "Updating: feature.xml"
-sed -i "s/$OLD_VERSION/$NEW_VERSION/" eclipsecs-sevntu-plugin-feature/feature.xml
+FILE=eclipsecs-sevntu-plugin/META-INF/MANIFEST.MF
+echo "Updating: "$FILE
+sed -i "s/Bundle-Version: $OLD_VERSION/Bundle-Version: $NEW_VERSION/" $FILE
+
+FILE=eclipsecs-sevntu-plugin-feature/feature.xml
+echo "Updating: "$FILE
+xmlstarlet ed --ps \
+    -u "/feature[@version='$OLD_VERSION']/@version" -v $NEW_VERSION \
+    -u "/feature/plugin[@version='$OLD_VERSION']/@version" -v $NEW_VERSION \
+    $FILE > $FILE.new
+mv $FILE.new $FILE
+#sed -i "s/$OLD_VERSION/$NEW_VERSION/" eclipsecs-sevntu-plugin-feature/feature.xml
+#xmlstarlet ed --ps -u "/feature[@version="$OLD_VERSION"]/@version" -v $NEW_VERSION eclipsecs-sevntu-plugin-feature/feature.xml
