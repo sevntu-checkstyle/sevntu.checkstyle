b80a97f92d3d181e1ac8052e86411cd342816397
Kariem Hussein <kariem.hussein@gmail.com>
2016-07-29 15:10:56 +0200

    Issue #464: Check for trailing commas on enums. Original PR: #465

---
+++ sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/EnumTrailingCommaAndSemicolonCheckTest.java
@@ -0,0 +1,42 @@
+package com.github.sevntu.checkstyle.checks.coding;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static com.github.sevntu.checkstyle.checks.coding.EnumTrailingCommaAndSemicolonCheck.MSG_KEY;
+import static com.github.sevntu.checkstyle.checks.coding.EnumTrailingCommaAndSemicolonCheck.MSG_KEY_SEMI;
+
+public class EnumTrailingCommaAndSemicolonCheckTest
+    extends BaseCheckTestSupport {
+    @Override
+    protected String getPath(String filename) throws IOException {
+        URL r = getClass().getResource(filename);
+        return new File(r.getPath()).getCanonicalPath();
+    }
+
+    @Test
+    public void testDefault() throws Exception {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(EnumTrailingCommaAndSemicolonCheck.class);
+        final String[] expected = {
+            "14: " + getCheckMessage(MSG_KEY),
+            "20: " + getCheckMessage(MSG_KEY),
+            "26: " + getCheckMessage(MSG_KEY_SEMI),
+        };
+        verify(checkConfig, getPath("InputEnumTrailingComma.java"), expected);
+    }
+
+    @Test
+    public void testTokensNotNull() {
+        final EnumTrailingCommaAndSemicolonCheck check = new EnumTrailingCommaAndSemicolonCheck();
+        Assert.assertNotNull(check.getAcceptableTokens());
+        Assert.assertNotNull(check.getDefaultTokens());
+        Assert.assertNotNull(check.getRequiredTokens());
+    }
+}
---
+++ sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/coding/InputEnumTrailingComma.java
@@ -0,0 +1,61 @@
+package com.github.sevntu.checkstyle.checks.coding;
+
+public interface InputEnumTrailingComma
+{
+    enum E1 {
+        ONE,
+        TWO,
+        THREE,
+    }
+
+    enum E2 {
+        ONE,
+        TWO,
+        THREE   // violation
+    }
+
+    enum E3 {
+        ONE,
+        TWO,
+        THREE;  // violation
+    }
+
+    enum E4 {
+        ONE,
+        TWO,
+        THREE,; // violation
+    }
+
+    enum E5 {
+        ONE,
+        TWO,
+        THREE,
+        ;
+    }
+
+    // enums below are ignored by the check, but were added for completenes
+    // Please don't remove, they are necessary for full cobertura branch coverage
+
+    // empty
+    enum E6 {}
+
+    // single enum const, single-line block
+    enum E7_1 { ONE }
+    enum E7_2 { ONE; }
+    enum E7_3 { ONE, }
+    enum E7_4 { ONE,; }
+
+    // single enum const, multi-line block
+    enum E8_1 {
+        ONE
+    }
+    enum E8_2 {
+        ONE;
+    }
+    enum E8_3 {
+        ONE,
+    }
+    enum E8_4 {
+        ONE,;
+    }
+}
