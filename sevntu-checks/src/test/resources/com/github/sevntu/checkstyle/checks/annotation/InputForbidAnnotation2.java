b97429d9814ba09338004d5be1f4b21391f39207
rnveach <rveach02@gmail.com>
2016-12-05 12:59:34 -0500

    Issue #520: 100% coverage for ForbidAnnotationCheck

--- sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/annotation/InputForbidAnnotation2.java
+++ sevntu-checks/src/test/resources/com/github/sevntu/checkstyle/checks/annotation/InputForbidAnnotation2.java
@@ -1,7 +1,7 @@
 package com.github.sevntu.checkstyle.checks.annotation;
-import java.awt.Component;
 
-public class InputForbidAnnotation2 extends Component {
-	private static final long serialVersionUID = 1L;
-
+public class InputForbidAnnotation2 {
+    @org.junit.Test
+    public void method() {
+    }
 }
