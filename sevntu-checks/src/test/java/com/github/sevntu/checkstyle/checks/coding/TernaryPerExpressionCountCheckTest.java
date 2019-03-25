bb55d3c282d572055509be82ba4c20ea173b8f7f
Roman Ivanov <ivanov-jr@mail.ru>
2019-03-25 06:41:13 -0700

    Issue #729: upgrade to checkstyle and eclipse-cs 8.18

--- sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/TernaryPerExpressionCountCheckTest.java
+++ sevntu-checks/src/test/java/com/github/sevntu/checkstyle/checks/coding/TernaryPerExpressionCountCheckTest.java
@@ -84,9 +84,9 @@ public class TernaryPerExpressionCountCheckTest extends AbstractModuleTestSuppor
         }
         catch (CheckstyleException ex) {
             final String errorMsg = ex.getMessage();
-            final String expectedMessage = "Cannot set property 'maxTernaryPerExpressionCount' "
-                + "to '-1' in module com.github.sevntu.checkstyle.checks.coding."
-                + "TernaryPerExpressionCountCheck";
+            final String expectedMessage = "cannot initialize module"
+                    + " com.github.sevntu.checkstyle.checks.coding.TernaryPerExpressionCountCheck -"
+                    + " Cannot set property 'maxTernaryPerExpressionCount' to '-1'";
             Assert.assertTrue("Expected error message is missing: " + expectedMessage,
                 errorMsg.contains(expectedMessage));
         }
