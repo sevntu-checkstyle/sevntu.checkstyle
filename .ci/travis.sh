e245e7361f251034e964cd9f16993356cf53bd7b
Roman Ivanov <ivanov-jr@mail.ru>
2020-01-22 19:24:58 -0800

    infra: move all clones to .ci-temp folder

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -63,21 +63,25 @@ sevntu-checks)
   ;;
 
 all-sevntu-checks-contribution)
+  mkdir -p .ci-temp
   wget -q \
     https://raw.githubusercontent.com/checkstyle/contribution/master/checkstyle-tester/checks-sevntu-error.xml
   xmlstarlet sel --net --template -m .//module -v "@name" -n checks-sevntu-error.xml \
     | grep -vE "Checker|TreeWalker|Filter|Holder" | grep -v "^$" \
     | sed "s/com\.github\.sevntu\.checkstyle\.checks\..*\.//" \
-    | sort | uniq | sed "s/Check$//" > web.txt
+    | sort | uniq | sed "s/Check$//" > .ci-temp/web.txt
   xmlstarlet sel --net --template -m .//module -v "@name" -n sevntu-checks/sevntu-checks.xml \
     | grep -vE "Checker|TreeWalker|Filter|Holder" | grep -v "^$" \
     | sed "s/com\.github\.sevntu\.checkstyle\.checks\..*\.//" \
-    | sort | uniq | sed "s/Check$//" > file.txt
-  diff -u web.txt file.txt
+    | sort | uniq | sed "s/Check$//" > .ci-temp/file.txt
+  diff -u .ci-temp/web.txt .ci-temp/file.txt
   ;;
 
 checkstyle-regression)
+  mkdir -p .ci-temp
+  cd .ci-temp
   git clone https://github.com/checkstyle/checkstyle
+  cd ../
   # update checkstyle_sevntu_checks.xml file in checkstyle for new modules
   cd sevntu-checks
   SEVNTU_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
@@ -88,10 +92,10 @@ checkstyle-regression)
                    --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   echo eclipse-cs version:$ECLIPSE_CS_VERSION
   mvn -e install -Pno-validations
-  mvn -e test -Dtest=CheckstyleRegressionTest#setupFiles -Dregression-path=../
+  mvn -e test -Dtest=CheckstyleRegressionTest#setupFiles -Dregression-path=../.ci-temp
   cd ../
   # execute checkstyle validation on updated config file
-  cd checkstyle
+  cd .ci-temp/checkstyle
   mvn -e clean verify -e -DskipTests -DskipITs -Dpmd.skip=true \
       -Dfindbugs.skip=true -Dcobertura.skip=true \
       -Dmaven.sevntu-checkstyle-check.checkstyle.version=$ECLIPSE_CS_VERSION \
