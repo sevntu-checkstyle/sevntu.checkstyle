b1f3071b0958adf37a88571a58d6c704eab48677
rnveach <rveach02@gmail.com>
2022-10-14 15:33:58 -0400

    Issue #883: completed upgrade to java 11

--- sevntu-checks/.ci/eclipse-compiler-javac.sh
+++ sevntu-checks/.ci/eclipse-compiler-javac.sh
@@ -12,20 +12,22 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
+JAVA_RELEASE=${3:-11}
+
 ECLIPSE_URL="http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/eclipse/downloads/drops4"
 ECJ_MAVEN_VERSION=$(wget --quiet -O- "$ECLIPSE_URL/?C=M;O=D" | grep -o "R-[^/]*" | head -n1)
 echo "Latest eclipse release is $ECJ_MAVEN_VERSION"
 ECJ_JAR=$(wget --quiet -O- "$ECLIPSE_URL/$ECJ_MAVEN_VERSION/" | grep -o "ecj-[^\"]*" | head -n1)
 ECJ_PATH=~/.m2/repository/$ECJ_MAVEN_VERSION/$ECJ_JAR
 
-if [ ! -f $ECJ_PATH ]; then
+if [ ! -f "$ECJ_PATH" ]; then
     echo "$ECJ_PATH is not found, downloading ..."
     cd target
-    wget $ECLIPSE_URL/$ECJ_MAVEN_VERSION/$ECJ_JAR
+    wget $ECLIPSE_URL/"$ECJ_MAVEN_VERSION"/"$ECJ_JAR"
     echo "test jar after download:"
-    jar -tvf $ECJ_JAR > /dev/null
-    mkdir -p $(dirname "$ECJ_PATH")
-    cp $ECJ_JAR $ECJ_PATH
+    jar -tvf "$ECJ_JAR" > /dev/null
+    mkdir -p "$(dirname "$ECJ_PATH")"
+    cp "$ECJ_JAR" "$ECJ_PATH"
     cd ..
 fi
 
@@ -39,10 +41,10 @@ mkdir -p target/classes target/test-classes target/eclipse
 RESULT_FILE=target/eclipse/report.txt
 
 echo "Executing eclipse compiler, output is redirected to $RESULT_FILE..."
-echo "java -jar $ECJ_PATH -target 1.8 -source 1.8 -cp $1  ..."
+echo "java -jar $ECJ_PATH -target ${JAVA_RELEASE} -source ${JAVA_RELEASE} -cp $1  ..."
 
 set +e
-java -jar $ECJ_PATH -target 1.8 -source 1.8 -encoding UTF-8 -cp $1 \
+java -jar "$ECJ_PATH" -target "${JAVA_RELEASE}" -source "${JAVA_RELEASE}" -encoding UTF-8 -cp "$1" \
         -d target/eclipse-compile \
         -properties org.eclipse.jdt.core.prefs \
         -enableJavadoc \
@@ -59,7 +61,7 @@ if [[ $EXIT_CODE != 0 ]]; then
 else
     # check compilation of resources, all WARN and INFO are ignored
     set +e
-    java -jar $ECJ_PATH -target 1.8 -source 1.8 -cp $1 \
+    java -jar "$ECJ_PATH" -target "${JAVA_RELEASE}" -source "${JAVA_RELEASE}" -cp "$1" \
             -d target/eclipse-compile \
             -nowarn \
             src/main/java \
@@ -75,3 +77,4 @@ else
       false
     fi
 fi
+
